a9947da0ef1e2b27d07c674990b049ff09df4e31
KyomaHooin <richard.bruna@protonmail.com>
2019-11-20 14:22:37 +0100

    Update code.

--- json-marc.py
+++ json-marc.py
@@ -7,24 +7,37 @@
 
 from __future__ import print_function
 
-import json,sys,os,re
+import argparse,json,sys,os,re
 
 from pymarc import JSONReader,JSONWriter,MARCWriter,XMLWriter
 
 # VAR -----------------
 
-IMPORT='import'
 LOG='json-marc.log'
 
-# INIT -----------------
+# DEF -----------------
 
-try:
-	os.mkdir('import')
-	os.mkdir('export')
-	os.mkdir('export/json')
-	os.mkdir('export/marc')
-	os.mkdir('export/xml')
-except: pass
+def valid_dir(s):
+	if os.path.isdir(s): return s
+	else:
+		msg = 'Invalid directory.'
+		raise argparse.ArgumentTypeError(msg)
+
+def valid_format(s):
+	if s in ('json', 'marc', 'xml'): return s
+	else:
+		msg = 'Invalid export format.'
+		raise argparse.ArgumentTypeError(msg)
+
+#  ARG -------------------
+
+parser = argparse.ArgumentParser(description="JSON - MARC Convertor.")
+required = parser.add_argument_group('required arguments')
+required.add_argument('--in', help='Import directory.', dest='in_dir', type=valid_dir, required=True)
+required.add_argument('--format', help='Export format. [json] [marc] [xml]', type=valid_format, required=True)
+args = parser.parse_args()
+
+# INIT -----------------
 
 try:
 	log = open(LOG,'a',0)
@@ -34,29 +47,45 @@ except:
 
 # MAIN -----------------
 
-for f in os.listdir(IMPORT):
+for f in os.listdir(args.in_dir):
+
+	# skip directory
+	if os.path.isdir(args.in_dir + '/' + f): continue
+
+	print(f)
 
 	# LOAD JSON
-	j = json.load(f, encoding='utf-8')
+	j = json.load(open(args.in_dir + '/' + f, 'rb'), encoding='utf-8')
 
 	# MODIFY STRUCTURE
 	print(json.dumps(j, indent=2))
 
-	# WRITEMARC
+	# EXPORT -----------------
+
+	try:
+		os.mkdir('export')
+		os.mkdir('export/' + args.format)
+	except: pass
+
 	reader = JSONReader(j)
+
+	print(reader)
+
+	sys.exit(1)
+
 	for record in reader:
-		# EXPORT MARC21
-		writer = MARCWriter(open('export/marc/' + re.sub('(.*)\.json', '\\1', f)  + '.dat', 'wb'))
-		writer.write(record)
-		writer.close()
-		# JSON
-		writer = JSONWriter(open('export/json/' + re.sub('(.*)\.json', '\\1', f) + '.json', 'wt'))
-		writer.write(record)
-		writer.close()
-		# MARCXML
-		writer = XMLWriter(open('export/xml/' + re.sub('(.*)\.json', '\\1', f) + '.xml', 'wb'))
-		writer.write(record)
-		writer.close()
+		if args.format == 'json':# JSON
+			writer = JSONWriter(open('export/json/' + re.sub('(.*)\.json', '\\1', f) + '.json', 'wt'))
+			writer.write(record)
+			writer.close()
+		if args.format == 'marc':# MARC21
+			writer = MARCWriter(open('export/marc/' + re.sub('(.*)\.json', '\\1', f)  + '.dat', 'wb'))
+			writer.write(record)
+			writer.close()
+		if args.format == 'xml':# MARCXML
+			writer = XMLWriter(open('export/xml/' + re.sub('(.*)\.json', '\\1', f) + '.xml', 'wb'))
+			writer.write(record)
+			writer.close()
 
 # EXIT -------------------
 
