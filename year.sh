227688cb53c1c6588319046a4641948db7ccb25e
KyomaHooin <richard.bruna@protonmail.com>
2020-01-06 17:17:35 +0100

    Fix A link, one day check.

--- year.sh
+++ year.sh
@@ -29,118 +29,145 @@ FOOTER='</tr>
 
 TEMPLATE='<td>
 <table>
-<tr><td align="right" width="80"><a style="text-decoration:none" href="jan.html">MONTH</a></td></tr>
+<tr><td align="right" width="80"><a style="text-decoration:none" href="MNT.html">MONTH</a></td></tr>
 <tr><td align="right" width="80">TOTAL</td></tr>
 <tr><td align="right" width="80">MATCH</td></tr>
 </table>
 </td>'
 
+#--------------------
+
+rm /var/www/html/*.html 2>/dev/null
 echo $HEADER >> /var/www/html/index.html
 
 #--------------------
 
-./oai-marc.py --set UCLA --from '2019-01-01 00:00:00' --until '2019-01-31 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-01-01 00:00:00' --until '2019-01-31 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-01-01 00:00:00' --until '2019-01-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/jan.html
 echo $TEMPLATE | \
+	sed "s/MNT/jan/" | \
 	sed "s/MONTH/Leden/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-./oai-marc.py --set UCLA --from '2019-02-01 00:00:00' --until '2019-02-28 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-02-01 00:00:00' --until '2019-02-28 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-02-01 00:00:00' --until '2019-02-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/feb.html
 echo $TEMPLATE | \
+	sed "s/MNT/feb/" | \
 	sed "s/MONTH/Únor/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-./oai-marc.py --set UCLA --from '2019-03-01 00:00:00' --until '2019-03-31 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-03-01 00:00:00' --until '2019-03-31 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-03-01 00:00:00' --until '2019-03-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/mar.html
 echo $TEMPLATE | \
+	sed "s/MNT/mar/" | \
 	sed "s/MONTH/Březen/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-./oai-marc.py --set UCLA --from '2019-04-01 00:00:00' --until '2019-04-30 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-04-01 00:00:00' --until '2019-04-30 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-04-01 00:00:00' --until '2019-04-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/apr.html
 echo $TEMPLATE | \
+	sed "s/MNT/apr/" | \
 	sed "s/MONTH/Duben/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-05-01 00:00:00' --until '2019-05-31 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-05-01 00:00:00' --until '2019-05-31 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-05-01 00:00:00' --until '2019-05-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/may.html
 echo $TEMPLATE | \
+	sed "s/MNT/may/" | \
 	sed "s/MONTH/Květen/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-06-01 00:00:00' --until '2019-06-30 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-06-01 00:00:00' --until '2019-06-30 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-06-01 00:00:00' --until '2019-06-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/jun.html
 echo $TEMPLATE | \
+	sed "s/MNT/jun/" | \
 	sed "s/MONTH/Červen/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-07-01 00:00:00' --until '2019-07-31 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-07-01 00:00:00' --until '2019-07-31 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-07-01 00:00:00' --until '2019-07-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/jul.html
 echo $TEMPLATE | \
+	sed "s/MNT/jul/" | \
 	sed "s/MONTH/Červenec/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-08-01 00:00:00' --until '2019-08-31 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-08-01 00:00:00' --until '2019-08-31 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-08-01 00:00:00' --until '2019-08-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/aug.html
 echo $TEMPLATE | \
+	sed "s/MNT/aug/" | \
 	sed "s/MONTH/Srpen/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-09-01 00:00:00' --until '2019-09-30 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-09-01 00:00:00' --until '2019-09-30 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-09-01 00:00:00' --until '2019-09-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/sep.html
 echo $TEMPLATE | \
+	sed "s/MNT/sep/" | \
 	sed "s/MONTH/Září/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-10-01 00:00:00' --until '2019-10-31 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-10-01 00:00:00' --until '2019-10-31 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-10-01 00:00:00' --until '2019-10-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/oct.html
 echo $TEMPLATE | \
+	sed "s/MNT/oct/" | \
 	sed "s/MONTH/Říjen/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-11-01 00:00:00' --until '2019-11-30 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-11-01 00:00:00' --until '2019-11-30 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-11-01 00:00:00' --until '2019-11-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/nov.html
 echo $TEMPLATE | \
+	sed "s/MNT/nov/" | \
 	sed "s/MONTH/Listopad/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
 rm stat.log
 sleep 300
 
-/oai-marc.py --set UCLA --from '2019-12-01 00:00:00' --until '2019-12-31 23:59:59' --check > stat.log 2>&1
+#./oai-marc.py --set UCLA --from '2019-12-01 00:00:00' --until '2019-12-31 23:59:59' --check > stat.log 2>&1
+./oai-marc.py --set UCLA --from '2019-12-01 00:00:00' --until '2019-12-01 23:59:59' --check > stat.log 2>&1
 mv oai-marc.html /var/www/html/dec.html
 echo $TEMPLATE | \
+	sed "s/MNT/dec/" | \
 	sed "s/MONTH/Prosinec/" | \
 	sed "s/TOTAL/$(grep TOTAL stat.log | cut -d\  -f2)/" | \
 	sed "s/MATCH/$(grep MATCH stat.log | cut -d\  -f2)/" >> /var/www/html/index.html
