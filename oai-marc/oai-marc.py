f113ef35e6873b8facc949300eeb7f7bbc4f8c72
KyomaHooin <richard.bruna@protonmail.com>
2020-01-08 13:14:37 +0100

    Fix 'ive' rule.

--- oai-marc/oai-marc.py
+++ oai-marc/oai-marc.py
@@ -710,7 +710,7 @@ for record in records:
 		if '100' in metadata:
 			if '4' in metadata['100']:
 				DATA = metadata['100'].get_subfields('4')
-				if 'aut' not in DATA or 'ive' not in DATA:
+				if 'aut' not in DATA and 'ive' not in DATA:
 					html_write(header.identifier(), '700', SIF, 'V poli 100 chybí kód role "aut" nebo "ive".')
 
 	# EXPORT -------------------
