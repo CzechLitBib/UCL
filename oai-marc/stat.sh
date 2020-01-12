05a6f7bb060039b40ee6f740c38eacc60226359e
KyomaHooin <richard.bruna@protonmail.com>
2020-01-12 11:58:13 +0000

    Error statistic HTML update.

--- oai-marc/stat.sh
+++ oai-marc/stat.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 #
-# ./stat.sh | sort -n -k3 | tac
+# Generate error code statictic
 #
 
-START_CODE=000
-END_CODE=123
+STAT='/var/www/html/stat.html'
 
-for I in $(eval echo {$START_CODE..$END_CODE}); do
-	COUNT=$(grep ">$I<" *.html 2>/dev/null | wc -l)
+DATA_PATH='/var/www/html/*.html'
+DATA=''
+
+CODE_START=000
+CODE_END=123
+
+HTML_HEADER='<html>
+<head><meta charset="utf-8"></head>
+<body style="background-color:black;">
+<pre><font size="4" color="white">'
+HTML_FOOTER='</font>
+</pre>
+</body>
+</html>'
+
+#-----------------
+
+for I in $(eval echo {$CODE_START..$CODE_END}); do
+	COUNT=$(grep ">$I<" $DATA_PATH 2>/dev/null | wc -l)
 	if [ $COUNT != 0 ]; then
-		echo "[$I] - $COUNT"
+		DATA=$(echo -ne "$DATA\n[$I] - $COUNT")
 	fi
 done
 
+echo "$HTML_HEADER" >$STAT
+echo "$DATA" | sort -n -k3 | tac >> $STAT
+echo "$HTML_FOOTER" >> $STAT
 
