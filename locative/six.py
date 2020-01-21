7c536f584062bd52e00a4e9a2dcd7cb2aa921ef7
KyomaHooin <richard.bruna@protonmail.com>
2020-01-21 16:48:31 +0100

    drop in, error handling.

--- locative/six.py
+++ locative/six.py
@@ -11,13 +11,14 @@ SERVER='lindat.mff.cuni.cz'
 URL='/services/morphodita/api/generate'
 HEADER={'Content-type':'application/x-www-form-urlencoded', 'Accept':'application/json'}
 
-with open(sys.argv[1], 'r') as f:
-	for line in f.readlines():
-		c = httplib.HTTPSConnection(SERVER, timeout=10)
-		c.request('POST', URL, urllib.urlencode({'data':line, 'output':'json'}), HEADER)
-		r = c.getresponse()
-		if r.status == 200:
-			for res in json.loads(r.read())['result'][0]:
-				if re.match('NN[N|F]S6', res['tag']):
-					print(res['form'])
-			
+try:
+	with open(sys.argv[1], 'r') as f:
+		for line in f.readlines():
+			c = httplib.HTTPSConnection(SERVER, timeout=10)
+			c.request('POST', URL, urllib.urlencode({'data':line, 'output':'json'}), HEADER)
+			r = c.getresponse()
+			if r.status == 200:
+				for res in json.loads(r.read())['result'][0]:
+					if re.match('NN[N|F]S6', res['tag']):
+						print(res['form'])
+except:	print('Error.')
