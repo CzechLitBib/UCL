71a69485c46bcc399477086d65c395cdcd603a6d
KyomaHooin <richard.bruna@protonmail.com>
2020-01-22 09:48:13 +0100

    Locative logging.

---
+++ locative/README.md
@@ -0,0 +1,17 @@
+
+DESCRIPTION
+
+Convert word to locative with MorphoDiTa REST API.
+
+USAGE
+<pre>
+./six.py in.txt
+</pre>
+FILE
+<pre>
+six.py - Simple code.
+</pre>
+SOURCE
+
+https://github.com/KyomaHooin/UCL
+
--- locative/six.py
+++ locative/six.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 #
-# Morphidita REST API client:
+# Simple Morphidita REST API client
 #
-# https://lindat.mff.cuni.cz/services/morphodita/api/generate?data=Brno&output=json
+# http://ufal.mff.cuni.cz/morphodita
 #
 
-import httplib,urllib,json,sys,re
+import httplib,urllib,json,time,sys,re
 
 SERVER='lindat.mff.cuni.cz'
 URL='/services/morphodita/api/generate'
 HEADER={'Content-type':'application/x-www-form-urlencoded', 'Accept':'application/json'}
+
+bad = open('six-bad.log', 'a', 0)
+out = open('six-out.log', 'a', 0)
 
 try:
 	with open(sys.argv[1], 'r') as f:
@@ -18,7 +21,18 @@ try:
 			c.request('POST', URL, urllib.urlencode({'data':line, 'output':'json'}), HEADER)
 			r = c.getresponse()
 			if r.status == 200:
-				for res in json.loads(r.read())['result'][0]:
-					if re.match('NN[N|F]S6', res['tag']):
-						print(res['form'])
-except:	print('Error.')
+				DATA = json.loads(r.read())['result'][0]
+				if len(DATA) == 0:
+					bad.write(line)
+				else:
+					for res in DATA:
+						if re.match('NN.S6', res['tag']):
+							out.write(line.strip() + ':' + res['form'].encode('utf-8') + '\n')
+			else:
+				print("Response error: " + str(r.status))
+			time.sleep(1)# HTTPS rate limiting
+except: print('Error.')
+
+bad.close()
+out.close()
+
