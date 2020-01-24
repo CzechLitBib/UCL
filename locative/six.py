59b4054aeb73a139683a86cafc22160919f150ca
KyomaHooin <richard.bruna@protonmail.com>
2020-01-24 10:34:55 +0100

    Overwrite, plural only, substring match fix.

--- locative/six.py
+++ locative/six.py
@@ -16,16 +16,16 @@ SERVER='lindat.mff.cuni.cz'
 URL='/services/morphodita/api/generate'
 HEADER={'Content-type':'application/x-www-form-urlencoded', 'Accept':'application/json'}
 
-bad = open('six-bad.log', 'a', 0)
-out = open('six-out.log', 'a', 0)
+bad = open('six-bad.log', 'w', 0)
+out = open('six-out.log', 'w', 0)
 
 #try:
 with open(sys.argv[1], 'r') as f:
 	for line in f.readlines():
 		suffix=''
-		if re.match('^.+ [pod|nad|u] .+$', line.strip()):# split multiline
-			suffix = re.sub('^.+( [pod|nad|u] .+)$','\\1', line.strip())
-			line = re.sub('^(.+) [pod|nad|u] .+$','\\1', line.strip())
+		if re.match('^.+ (pod|nad|u) .+$', line.strip()):# split multiline
+			suffix = re.sub('^.+( (pod|nad|u) .+)$','\\1', line.strip())
+			line = re.sub('^(.+) (pod|nad|u) .+$','\\1', line.strip())
 		c = httplib.HTTPSConnection(SERVER, timeout=10)
 		c.request('POST', URL, urllib.urlencode({'data':line, 'output':'json'}), HEADER)
 		r = c.getresponse()
@@ -34,9 +34,14 @@ with open(sys.argv[1], 'r') as f:
 			if len(DATA) == 0:
 				bad.write(line)
 			else:
-				HAS_U,HAS_E = False, False
-				LOCATIVE = [res['form'] for res in DATA if re.match('NN.[S|X][6|X]', res['tag'])]# locative
-				if LOCATIVE:# plural
+				HAS_U,HAS_E,HAS_S = False, False, False
+				for tag in [res['tag'] for res in DATA]:
+					if re.match('...S.', tag): HAS_S = True
+				if HAS_S:
+					LOCATIVE = [res['form'] for res in DATA if re.match('NN.S6', res['tag'])]# singular
+				else:
+					LOCATIVE = [res['form'] for res in DATA if re.match('NN.[P|X][6|X]', res['tag'])]# plural
+				if LOCATIVE:
 					LOCATIVE = [L for L in LOCATIVE if not re.match('.*ovi$', L)]# match 'ovi'
 					for L in LOCATIVE:# match 'e/ě'
 						if re.match('.*u$', L.encode('utf-8')): HAS_U = True
