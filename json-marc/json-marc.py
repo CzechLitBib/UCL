b2cb3e87392d07ee401447bd09a61648f185b052
KyomaHooin <kyomahooin@gmail.com>
2020-09-14 16:26:50 +0200

    Retro update.

--- json-marc/json-marc.py
+++ json-marc/json-marc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
 # JSON to MARC convertor.
 # 
@@ -9,81 +10,81 @@ from __future__ import print_function
 
 import argparse,json,sys,os,re
 
-from pymarc import JSONReader,JSONWriter,MARCWriter,XMLWriter
-from pymarc.record import Record
+from pymarc import Record
 from pymarc.field import Field
+
+# fast JSON streaming library
+#from yajl import YajlParser,YajlContentHandler
 
 # VAR -----------------
 
-LOG='json-marc.log'
-
-# DEF -----------------
-
-def valid_dir(s):
-	if os.path.isdir(s): return s
-	else:
-		msg = 'Invalid directory.'
-		raise argparse.ArgumentTypeError(msg)
-
-def valid_format(s):
-	if s in ('json', 'marc', 'xml'): return s
-	else:
-		msg = 'Invalid export format.'
-		raise argparse.ArgumentTypeError(msg)
-
-#  ARG -------------------
-
-#parser = argparse.ArgumentParser(description="JSON - MARC Convertor.")
-#required = parser.add_argument_group('required arguments')
-#required.add_argument('--in', help='Import directory.', dest='in_dir', type=valid_dir, required=True)
-#required.add_argument('--format', help='Export format. [json] [marc] [xml]', type=valid_format, required=True)
-#args = parser.parse_args()
+#IN='retrobi.json'
+IN='demo.json'
+#BUFFER={}
 
 # INIT -----------------
 
-#try:
-#	log = open(LOG,'a',0)
-#except:
-#	print('Read only FS exiting.')
-#	sys.exit(1)
+record = Record()
+
+record.leader = '     nab a22     4a 4500'
+#record.add_ordered_field(Field(tag='FMT', data='RS'))
+record.add_ordered_field(Field(tag='003', data='CZ PrUCL'))
+record.add_ordered_field(Field(tag='005', data='20201231'))
+record.add_ordered_field(Field(tag='040', indicators=[' ',' '], subfields=['a', 'ABB060','b', 'cze']))
+record.add_ordered_field(Field(tag='041', indicators=['0',' '], subfields=['a', 'cze']))
+record.add_ordered_field(Field(tag='336', indicators=[' ',' '], subfields=['a', 'text', 'b', 'txt', '2', 'rdacontent']))
+record.add_ordered_field(Field(tag='337', indicators=[' ',' '], subfields=['a', u'bez média', 'b', 'n', '2', 'rdamedia']))
+record.add_ordered_field(Field(tag='338', indicators=[' ',' '], subfields=['a', u'jiný', 'b', 'nz', '2', 'rdacarrier']))
+record.add_ordered_field(Field(tag='500', indicators=[' ',' '], subfields=['a', u'Strojově převedený záznam z RETROBI bez redakční kontroly.']))
+record.add_ordered_field(Field(tag='910', indicators=[' ',' '], subfields=['a', 'ABB060']))
+record.add_ordered_field(Field(tag='964', indicators=[' ',' '], subfields=['a', 'RETROBI']))
+record.add_ordered_field(Field(tag='OWN', indicators=[' ',' '], subfields=['a', 'UCLA']))
+#record.add_ordered_field(Field(tag='SIF', data='RET'))
+
+# DEF -----------------
+
+#class ContentHandler(YajlContentHandler):
+#	def __init__(self): pass
+#	def yajl_null(self, ctx): pass
+#	def yajl_start_map(self, ctx): pass
+#	def yajl_end_map(self, ctx): pass
+#	def yajl_start_array(self, ctx): pass
+#	def yajl_end_array(self, ctx): pass
+#
+#	def yajl_boolean(self, ctx, boolVal):
+#		field_complete(['bool', boolVal])
+#	def yajl_integer(self, ctx, integerVal):
+#		field_complete(['integer', integerVal])
+#	def yajl_double(self, ctx, doubleVal):
+#		field_complete(['double', doubleVal])
+#	def yajl_number(self, ctx, stringNum):
+#		field_complete(['number', stringNum])
+#	def yajl_string(self, ctx, stringVal):
+#		field_complete(['string', stringVal])
+#	def yajl_map_key(self, ctx, stringVal):
+#		field_complete(['key', stringVal])
+#
+#def field_complete(tpl):
+#	print(tpl[0] + ': ' + str(tpl[1]))
 
 # MAIN -----------------
 
-# LOAD BROKEN JSON SOURCE
+#parser = YajlParser(ContentHandler())
+#parser.dont_validate_strings=True
+#parser.allow_multiple_values=True
 
-with open('in/in2.json','rb') as f:
-	j = json.loads(f.read(), strict=False)# ignore control char garbage
+with open(IN, 'rb') as f:
+	for LINE in f:
+		j = json.loads(re.sub('(.*),$','\\1',LINE.strip()), strict=False)
+		print(json.dumps(j, indent=2))
+		# ID
+		#record.add_ordered_field(Field(tag='001', data='RET-' +  j['id']))
+		# 
 
-#print(json.dumps(j, indent=2))
-#print(j['_rev'])
-#print(j['drawer'])
+#parser.parse(f=j)
+#j.close()
 
-# UPDATE TEMPLATE
-
-# THE JSON WAY
-#
-#          Core: {'fields':[], 'leader':'foo'}
-#   Basic field: {'001':'foo'}
-# Complex field: {'040':{'ind1':'', 'ind2':'', 'subfields':[{'a':''}, {'b':''}]}}
-#
-
-template = {'leader':'foo', 'fields':[{'001':'foo'}]}
-reader = JSONReader(json.dumps(template))
-
-for record in reader: print(record)
-
-# THE RECORD WAY
-#
-# field = Field(tag='001',data='foo')
-# field = Field(tag='040', indicators=['1','0'], subfields=['a','','b',''])
-#
-
-record = Record()
-record.leader = 'foo'
-field = Field(tag='001', data='foo')
-record.add_field(field)
-
-print(record)
+#print(record.as_json())
 
 # EXPORT -----------------
 
