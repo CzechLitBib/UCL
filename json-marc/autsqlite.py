2f6e9202feb46265bd3079369ad7bda10b67f021
KyomaHooin <kyomahooin@gmail.com>
2020-09-18 12:41:40 +0200

    Aleph to SQLite update.

--- json-marc/autsqlite.py
+++ json-marc/autsqlite.py
@@ -17,23 +17,27 @@ OUT='AUT.db'
 
 con = sqlite3.connect(OUT)# sqlite:///your_filename.db
 cur = con.cursor()
-cur.execute("CREATE TABLE t (id, a, d, seven);") # use your column names here
+cur.execute("CREATE TABLE t (id,a,b,c,d,q,w,zero,two,seven);") # use your column names here
 
 with open(IN, 'r') as f:
 	for line in f:
 		buff=[]
-		ID,A,D,SEVEN='','','',''
-
+		ID,A,B,C,D,Q,W,ZERO,TWO,SEVEN='','','','','','','','','',''
 		data = line.strip().split('$$')
 		ID = data[0].split(' ')[0]
+		del data[0]# zero clash
 		for val in data:
 			if re.match('^a', val): A = re.sub('^a(.*)', '\\1', val).decode('utf-8')
+			if re.match('^b', val): B = re.sub('^b(.*)', '\\1', val).decode('utf-8')
+			if re.match('^c', val): C = re.sub('^c(.*)', '\\1', val).decode('utf-8')
 			if re.match('^d', val): D = re.sub('^d(.*)', '\\1', val).decode('utf-8')
+			if re.match('^q', val): Q = re.sub('^q(.*)', '\\1', val).decode('utf-8')
+			if re.match('^w', val): W = re.sub('^w(.*)', '\\1', val).decode('utf-8')
+			if re.match('^0', val): ZERO = re.sub('^0(.*)', '\\1', val).decode('utf-8')
+			if re.match('^2', val): TWO = re.sub('^2(.*)', '\\1', val).decode('utf-8')
 			if re.match('^7', val): SEVEN = re.sub('^7(.*)', '\\1', val)
-		buff=[ID, A, D, SEVEN]
-		#print(buff)
-		#sys.exit(1)
-		cur.execute("INSERT INTO t (id, a, d, seven) VALUES (?, ?, ?, ?);", buff)
+		buff=[ID,A,B,C,D,Q,W,ZERO,TWO,SEVEN]
+		cur.execute("INSERT INTO t (id,a,b,c,d,q,w,zero,two,seven) VALUES (?,?,?,?,?,?,?,?,?,?);", buff)
 
 con.commit()
 con.close()
