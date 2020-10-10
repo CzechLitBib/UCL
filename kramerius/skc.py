fe50016e7dd5348e6bc3d9949334d583df4819de
KyomaHooin <kyomahooin@gmail.com>
2020-10-10 15:56:48 +0200

    Ramdisk, tune.

--- kramerius/skc.py
+++ kramerius/skc.py
@@ -3,7 +3,7 @@
 
 from __future__ import print_function
 
-import subprocess,requests,lxml.html,StringIO,os
+import subprocess,requests,lxml.html,StringIO,time,os
 from pymarc import MARCReader
 
 ISSN='demo.csv'
@@ -20,6 +20,7 @@ def check_issn(issn,name):
 	global session
 	print('Trying.. ' + issn)
 	scrap.write('Trying .. ' + issn + ' ' + name + '\n')
+	os.chdir('ramdisk')
 	# create config, bin
 	with open('z3950.cfg', 'w') as f:
 		f.write('open tcp:aleph.nkp.cz:9991/SKC-UTF\n')
@@ -34,6 +35,7 @@ def check_issn(issn,name):
 	reader = MARCReader(open('rec.bin','rb'))
 	for rec in reader:
 		if '998' in rec:
+			print(URL_HEAD + rec['998'].value() + URL_TAIL)
 			req = session.get(URL_HEAD + rec['998'].value() + URL_TAIL)
 			if req.status_code == 200:
 				p = lxml.html.HTMLParser()
@@ -44,9 +46,11 @@ def check_issn(issn,name):
 						scrap.write('->' + o[i+1].text.encode('utf-8') + '\n')
 			else:
 				print("HTTPS error.")
+			time.sleep(1)# hold on a second
 	# cleanup
 	os.remove('z3950.cfg')		
 	os.remove('rec.bin')
+	os.chdir('../')
 
 scrap = open(OUT, 'w')
 
