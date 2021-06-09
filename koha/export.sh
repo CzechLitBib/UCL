f7384050100de0dc796ac63fa6e90d43eabebb9e
KyomaHooin <kyomahooin@gmail.com>
2021-06-09 11:55:00 +0200

    Koha CLI export 2.

--- koha/export.sh
+++ koha/export.sh
@@ -2,8 +2,10 @@
 #
 # CLI Koha export
 #
+# Type: auths | bibs
+#
 export PERL5LIB='/usr/share/koha/lib'
 export KOHA_CONF='/etc/koha/sites/koha/koha-conf.xml'
 
-perl /usr/share/koha/bin/export_records.pl --record-type='auths' --filename='koha_clo.xml'
+perl /usr/share/koha/bin/export_records.pl --record-type='auths' --format='xml' --filename='koha.xml'
 
