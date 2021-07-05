f63a90d3900a4308c545b167b4b818f4f7147b7a
KyomaHooin <kyomahooin@gmail.com>
2021-07-05 09:42:04 +0200

    API init

---
+++ api/api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#
+# MARCXML REST API
+#
+
+#import pymarc
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+#class Record(Resource):
+#    def get(sef):
+#        return Record(ident);
+
+#class ListRecords(Resource):
+#    def get(self)
+#        return Record();
+
+api.add_resource(HelloWorld, '/api')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
