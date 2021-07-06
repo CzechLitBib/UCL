d5167446efe3ec241882c32283d9fa1b5533cb59
KyomaHooin <kyomahooin@gmail.com>
2021-07-06 12:37:48 +0200

    Error anchor; robots.

--- oai-marc/html/error/index.php
+++ oai-marc/html/error/index.php
@@ -22,17 +22,18 @@ if(empty($_SESSION['auth'])) {
 $db = new SQLite3('error.db');
 
 if (!$db) {
-	echo '<p><font color="red">DB error.</font></p>';
+	echo "<p><font color='red'>DB error.</font></p>";
 } else {
 
 	$result = $db->query("SELECT id,label,text FROM error");
 	
 	while ($res = $result->fetchArray(SQLITE3_ASSOC)) {
-		echo '<p><table cellpadding="5" width="700">';
-		echo '<tr><td width="42" align="center"><font color="red"><b>' . $res['id'] . '</b></font></td><td width="650"><b>' . $res['label'] . '</b></td></tr>';
-		echo '<tr><td></td><td width="650"><div align="justify">' . $res['text'] . '</div></td></tr>';
-		echo '</table></p>';
-		echo '<p><hr width="650" style=" border:none; height:1px; background-color: black;"></p>';
+		echo "<a id='" . $res['id'] ."'></a>\n";
+		echo "<p><table cellpadding='5' width='700'>\n";
+		echo "<tr><td width='42' align='center'><font color='red'><b>" . $res['id'] . "</b></font></td><td width='650'><b>" . $res['label'] . "</b></td></tr>\n";
+		echo "<tr><td></td><td width='650'><div align='justify'>" . $res['text'] . "</div></td></tr>\n";
+		echo "</table></p>\n";
+		echo "<p><hr width='650' style='border: none;height: 1px;background-color: black;'></p>\n";
 	}
 	
 	$db->close();
