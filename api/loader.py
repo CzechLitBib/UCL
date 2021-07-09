1151cd950000810c1aac346324c6fb5800600494
KyomaHooin <kyomahooin@gmail.com>
2021-07-09 21:13:51 +0200

    Loader done.

--- api/loader.py
+++ api/loader.py
@@ -5,18 +5,7 @@
 
 import argparse,io
 
-from datetime import datetime
-
-from pymarc import marcxml,MARCWriter,XMLWriter
-
-#from xml.etree.ElementTree import ElementTree
-
-#import xml.etree.ElementTree as ET
-
-#ET.register_namespace('marc', 'http://www.loc.gov/MARC21/slim')
-
-#tree = ET.ElementTree()
-
+from pymarc import marcxml
 from datetime import datetime
 from lxml.etree import tostring
 from oaipmh.client import Client
@@ -27,41 +16,16 @@ SET='UCLA'
 
 # DEF -------------------
 
-def valid_date(s):
+def valid_date(date):
 	try:
-		return datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
+		return datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 	except:
 		raise argparse.ArgumentTypeError('Invalid date format.')
 
-def XMLParse(xml):
-	#handler = marcxml.XmlHandler()
-	#marcxml.parse_xml(io.StringIO(ET.Element.tostring(xml, encoding='utf-8').decode('utf-8')), handler)
-	#print(xml)
-
-	#return xml.getiterator()
-	return xml.getchildren()
-	#return lxml.etree(xml)	
-
-	#return ElementTree(xml)
-	#return tree.parse(xml)
-
-	#marcxml.record_to_xml_node(io.StringIO(tostring(xml, encoding='utf-8').decode('utf-8')), handler)
-	#return data
-	#return xml.findall('.//record')
-	#return parse(io.StringIO(tostring(xml, encoding='utf-8').decode('utf-8')))
-#	data = parse(tostring(xml, encoding='utf-8'))
-#	data = parse(xml)
-#	print(tostring(xml, encoding='utf-8'))
-#	data =type() xml.find('record')
-#	print(tostring(data))
-#	#data = parse(xml)
-#	buff = parse(xml).xpath('//record')[0]
-#	return tostring(data)
-
-#def MarcXML(xml):
-	#handler = marcxml.XmlHandler()
-	#marcxml.parse_xml(io.StringIO(tostring(xml, encoding='utf-8'))), handler)
-	#return handler.records[0]
+def MarcXML(xml):
+	handler = marcxml.XmlHandler()
+	marcxml.parse_xml(io.StringIO(tostring(xml).decode('utf-8')), handler)
+	return handler.records[0]
 
 # ARGS -------------------
 
@@ -78,7 +42,7 @@ if not args.until_date: parser.error('argument --until is required.')
 # OAI -------------------
 
 registry = MetadataRegistry()
-registry.registerReader('marc21', XMLParse)
+registry.registerReader('marc21', MarcXML)
 
 oai = Client(URL, registry)
 
@@ -87,16 +51,10 @@ records = oai.listRecords(metadataPrefix='marc21', set=SET, from_=args.from_date
 # MAIN -------------------
 
 for record in records:
-	print(record[1][0])
-	#print(tostring(record[1][0]))
-	#for i in record[1]:
-	#	print(i)
-	#print(record[1].find('record'))
-	#metadata = record[1]
-	#metadata.remove_fields('LDR')# DUP
-	#metadata.remove_fields('SYS')# Invalid.
-	#data = marcxml.record_to_xml_node(metadata, namespace=False)
-	#print(tostring(data))
-	#break
-
+	metadata = record[1]
+	metadata.remove_fields('LDR')# LDR DUP
+	metadata.remove_fields('SYS')# INVALID
+	print(marcxml.record_to_xml(metadata))
+	print(metadata.as_json())
+	print(metadata.as_marc())
 
