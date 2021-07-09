406916dcb9f143f79e691823167631fae1fde5ed
KyomaHooin <kyomahooin@gmail.com>
2021-07-09 20:18:42 +0200

    xml = lxml.etree._Element

--- api/loader.py
+++ api/loader.py
@@ -9,8 +9,16 @@ from datetime import datetime
 
 from pymarc import marcxml,MARCWriter,XMLWriter
 
+#from xml.etree.ElementTree import ElementTree
+
+#import xml.etree.ElementTree as ET
+
+#ET.register_namespace('marc', 'http://www.loc.gov/MARC21/slim')
+
+#tree = ET.ElementTree()
+
 from datetime import datetime
-from lxml.etree import tostring, parse
+from lxml.etree import tostring
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry
 
@@ -26,9 +34,19 @@ def valid_date(s):
 		raise argparse.ArgumentTypeError('Invalid date format.')
 
 def XMLParse(xml):
-	handler = marcxml.XmlHandler()
-	marcxml.parse_xml(io.StringIO(tostring(xml, encoding='utf-8').decode('utf-8')), handler)
-	return handler.records
+	#handler = marcxml.XmlHandler()
+	#marcxml.parse_xml(io.StringIO(ET.Element.tostring(xml, encoding='utf-8').decode('utf-8')), handler)
+	#print(xml)
+
+	#return xml.getiterator()
+	return xml.getchildren()
+	#return lxml.etree(xml)	
+
+	#return ElementTree(xml)
+	#return tree.parse(xml)
+
+	#marcxml.record_to_xml_node(io.StringIO(tostring(xml, encoding='utf-8').decode('utf-8')), handler)
+	#return data
 	#return xml.findall('.//record')
 	#return parse(io.StringIO(tostring(xml, encoding='utf-8').decode('utf-8')))
 #	data = parse(tostring(xml, encoding='utf-8'))
@@ -69,4 +87,16 @@ records = oai.listRecords(metadataPrefix='marc21', set=SET, from_=args.from_date
 # MAIN -------------------
 
 for record in records:
-	print(record[1])
+	print(record[1][0])
+	#print(tostring(record[1][0]))
+	#for i in record[1]:
+	#	print(i)
+	#print(record[1].find('record'))
+	#metadata = record[1]
+	#metadata.remove_fields('LDR')# DUP
+	#metadata.remove_fields('SYS')# Invalid.
+	#data = marcxml.record_to_xml_node(metadata, namespace=False)
+	#print(tostring(data))
+	#break
+
+
