ff524c990cf5fe9a98b2f99afb5de46f6c95ea96
KyomaHooin <kyomahooin@gmail.com>
2021-07-15 10:10:07 +0200

    KAT update.

--- oai-marc/mod/kat.py
+++ oai-marc/mod/kat.py
@@ -10,27 +10,27 @@ from datetime import datetime,timedelta
 # VAR -------------------
 
 KAT_CODE='/usr/local/bin/code/kat.txt'
-LAST_MONTH=(datetime.today()-timedelta(days=1)).strftime('%y%m')
-DATA_DIR='/var/www/html/kat/data/' + (datetime.today()-timedelta(days=1)).strftime('%Y/%m')
+
+JSON='/var/www/html/kat/data/' + (datetime.today()-timedelta(days=1)).strftime('%Y/%m/') + 'data.json'
 
 # DEF -------------------
 
 def get_sif_map():
-        try:
-                kat_code = {}
-                with open(KAT_CODE, 'r') as f:
-                        for line in f:
-                                acct_name, acct_code = line.split('#')
-                                sif_code[acct_name] = acct_code.strip()
-                return kat_code
-        except:
-                return {}
+	try:
+		sif_code = {}
+		with open(KAT_CODE, 'r') as f:
+			for line in f:
+				user_name, user_code = line.split('#')
+				sif_code[user_name] = user_code.strip()
+		return sif_code
+	except:
+		return {}
 
-def get_kat(record):
+def get_cat(record):
 	out = []
 	for F in record.get_fields('CAT','KAT'):
 		if 'a' in F and 'BATCH' not in F['a']:# not a bot
-			if 'c' in F and F['c'][0:6] == LAST_MONTH.replace('/',''):
+			if 'c' in F and F['c'][0:6] == (datetime.today()-timedelta(days=1)).strftime('%y%m'):
 				out.append(F['a'])
 	return out
 
@@ -41,26 +41,29 @@ def get_key(val,lst):
 
 def run(DATA):
 
-	BUFF={}
-
-	# initialize data
-	for key in kat_map():
-		BUFF[key] = {
-			'sif_count':0,
-			'cat_count':0,
-			'sif_cat_count':0,
-			'other': {},
-			'cat_id':[]
-		}
+	buff={}
 
 	# SIF/CAT map
 	sif_cat_map = get_sif_map()
 
+	# initialize data
+	for sif_parent in sif_cat_map:
+		buff[sif_parent] = {
+			'sif_count': 0,
+			'cat_count': 0,
+			'sif_cat_count': 0,
+			'other': {},
+			'ident': []
+		}
+		for sif_child in sif_cat_map:
+			if sif_parent != sif_child:
+				buff[sif_parent]['other'][sif_child] = 0
+
 	for record in DATA:
 
 		# ident
-		record['001'].value()
-	
+		ident = record['001'].value()
+
 		# SIF
 		SIF=''
 		if 'SIF' in record and 'a' in record['SIF']:
@@ -68,34 +71,38 @@ def run(DATA):
 
 		# SIF count
 		if SIF in sif_cat_map:
-			BUFF[SIF]['sif_count']+=1
+			buff[SIF]['sif_count']+=1
 
 		# KAT count	
-		for cat in get_cat_list(record):
-			if cat in sif_cat_map.values():
-				if SIF in sif_cat_map:
+		for cat in get_cat(record):
+			if cat in sif_cat_map:
+				if SIF in sif_cat_map.values():
 					# SELF
-					if SIF == get_key(cat,SIF_MAP):
+					if SIF == get_key(cat,sif_cat_map):
 						# KAT count
-						DATA[SIF]['cat_count']+=1
+						buff[SIF]['cat_count']+=1
 						# SIF/KAT count
-						DATA[SIF]['sif_cat_count']+=1
-						# SIF ID
-						if ALEPH not in DATA[SIF]['cat_id']:
-							DATA[SIF]['cat_id'].append(ALEPH)
+						buff[SIF]['sif_cat_count']+=1
+						# IDENT
+						if ident not in buff[SIF]['ident']:
+							buff[SIF]['ident'].append(ident)
 					# OTHER
 					else:
 						# KAT count
-						DATA[get_key(cat,SIF_MAP)]['cat_count']+=1
+						buff[get_key(cat,sif_cat_map)]['cat_count']+=1
 						# OTHER count
-						DATA[SIF]['other'][get_key(cat,SIF_MAP)]+=1
-						# SIF ID
-						if ALEPH not in DATA[get_key(cat,SIF_MAP)]['cat_id']:
-							DATA[get_key(cat,SIF_MAP)]['cat_id'].append(ALEPH)
+						buff[SIF]['other'][get_key(cat,sif_cat_map)]+=1
+						# IDENT
+						if ident not in buff[get_key(cat,sif_cat_map)]['ident']:
+							buff[get_key(cat,sif_cat_map)]['ident'].append(ident)
 				else:
 					# KAT count
-					DATA[get_key(cat,SIF_MAP)]['cat_count']+=1
-					# KAT ID
-					if not ALEPH in DATA[get_key(cat,SIF_MAP)]['cat_id']:
-						DATA[get_key(cat,SIF_MAP)]['cat_id'].append(ALEPH)
+					buff[get_key(cat,sif_cat_map)]['cat_count']+=1
+					# IDENT
+					if not ident in buff[get_key(cat,sif_cat_map)]['ident']:
+						buff[get_key(cat,sif_cat_map)]['ident'].append(ident)
+
+	# JSON
+	with open(JSON, 'w') as f:
+		json.dump(buff, f)
 
