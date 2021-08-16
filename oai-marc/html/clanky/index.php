a7b0bac4ad0099de64014bd2314a2cbb3c238876
KyomaHooin <kyomahooin@gmail.com>
2021-08-16 11:22:07 +0200

    Clanky update.

--- oai-marc/html/clanky/index.php
+++ oai-marc/html/clanky/index.php
@@ -3,7 +3,14 @@
 session_start();
 
 $id = uniqid();
-$value = ['author','name','source','quote','place','publisher','year','link','public'];
+
+$common = ['note','link','public'];
+
+$type['article'] = ['article-author','article-name','article-source','article-quote'];
+$type['chapter'] = ['chapter-author','chapter-name','chapter-src-author','chapter-src-name',
+		'chapter-src-place','chapter-src-publisher','chapter-src-year'];
+$type['book'] = ['book-author','book-name','book-place','book-publisher','book-year'];
+
 $output='';
 
 // validation
@@ -15,9 +22,12 @@ if (isset($_POST['code']) and isset($_SESSION['secret'])) {
 if ($valid) {
 	// CSV
 	$output .= $id . '|';
-	foreach($value as $val) {
-		if (isset($_POST[$val])) {
-			$val == 'public' ? $output .= $_POST[$val] : $output .= $_POST[$val] . '|';
+	foreach($type[$_POST['type']] as $v) {
+		isset($_POST[$v]) ? $output .= $_POST[$v] . '|' : $output .= '|';
+	}
+	foreach($common as $v) {
+		if (isset($_POST[$v])) {
+			$v == 'public' ? $output .= $_POST[$v] : $output .= $_POST[$v] . '|';
 		} else {
 			$output .= '|';
 		}
@@ -41,24 +51,24 @@ if ($valid) {
 <head>
 <script>
 	function on_load() {
-		document.getElementById("review-div").style.display = "block";
-		document.getElementById("bookpart-div").style.display = "none";
+		document.getElementById("article-div").style.display = "block";
+		document.getElementById("chapter-div").style.display = "none";
 		document.getElementById("book-div").style.display = "none";
 	}
 	function load_type() {
-		if (document.getElementById('review').checked) {
-			document.getElementById("review-div").style.display = "block";
-			document.getElementById("bookpart-div").style.display = "none";
+		if (document.getElementById('article').checked) {
+			document.getElementById("article-div").style.display = "block";
+			document.getElementById("chapter-div").style.display = "none";
 			document.getElementById("book-div").style.display = "none";
 		}
-		if (document.getElementById('bookpart').checked) {
-			document.getElementById("review-div").style.display = "none";
-			document.getElementById("bookpart-div").style.display = "block";
+		if (document.getElementById('chapter').checked) {
+			document.getElementById("article-div").style.display = "none";
+			document.getElementById("chapter-div").style.display = "block";
 			document.getElementById("book-div").style.display = "none";
 		}
 		if (document.getElementById('book').checked) {
-			document.getElementById("review-div").style.display = "none";
-			document.getElementById("bookpart-div").style.display = "none";
+			document.getElementById("article-div").style.display = "none";
+			document.getElementById("chapter-div").style.display = "none";
 			document.getElementById("book-div").style.display = "block";
 		}
 	}
@@ -71,60 +81,60 @@ if ($valid) {
 
 <p><hr style="border-top: 0px; border-bottom:1px solid black;" width="500"></p>
 
-<p><table><tr><td>
-<input type="radio" name="datatype" id="review" value="review" onclick="load_type()" checked><label>Článek</label>
-<input type="radio" name="datatype" id="bookpart" value="bookpart" onclick="load_type()"><label>Kapitola v knize</label>
-<input type="radio" name="datatype" id="book" value="book" onclick="load_type()"><label>Kniha</label>
-</td></tr></table></p>
-
 <form method="post" action="." enctype="multipart/form-data">
 
-<div id="review-div">
+<p><table><tr><td>
+<input type="radio" name="type" id="article" value="article" onclick="load_type()" checked><label>Článek</label>
+<input type="radio" name="type" id="chapter" value="chapter" onclick="load_type()"><label>Kapitola v knize</label>
+<input type="radio" name="type" id="book" value="book" onclick="load_type()"><label>Kniha</label>
+</td></tr></table></p>
+
+<div id="article-div">
 <table width="550">
 <tr><td width="175" align="right"><u><b>Základní údaje</b></u></td></tr>
 <tr height="8px"></tr>
-<tr><td align="right">Autor:</td><td><input type="text" name="review-author" size="20" value="
-<?php if (!$valid and isset($_POST['author'])) { echo htmlspecialchars($_POST['author'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Autor:</td><td><input type="text" name="article-author" size="20" value="
+<?php if (!$valid and isset($_POST['article-author'])) { echo htmlspecialchars($_POST['article-author'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
-<tr><td align="right">Název:</td><td><input type="text" name="review-name" size="20" value="
-<?php if (!$valid and isset($_POST['name'])) { echo htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8'); } ?>
-" required></td></tr>
-<tr><td align="right">Zdrojový dokument:</td><td><input type="text" name="review-source" size="20" value="
-<?php if (!$valid and isset($_POST['source'])) { echo htmlspecialchars($_POST['source'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Název:</td><td><input type="text" name="article-name" size="20" value="
+<?php if (!$valid and isset($_POST['article-name'])) { echo htmlspecialchars($_POST['article-name'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
-<tr><td align="right">Bibliografická citace:</td><td><input type="text" name="review-quote" size="30" value="
-<?php if (!$valid and isset($_POST['quote'])) { echo htmlspecialchars($_POST['quote'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Zdrojový dokument:</td><td><input type="text" name="article-source" size="20" value="
+<?php if (!$valid and isset($_POST['article-source'])) { echo htmlspecialchars($_POST['article-source'], ENT_QUOTES, 'UTF-8'); } ?>
+"></td></tr>
+<tr><td align="right">Bibliografická citace:</td><td><input type="text" name="article-quote" size="30" value="
+<?php if (!$valid and isset($_POST['article-quote'])) { echo htmlspecialchars($_POST['article-quote'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
 </table>
 </div>
 
-<div id="bookpart-div">
+<div id="chapter-div">
 <table width="550">
 <tr><td width="175" align="right"><u><b>Text</b></u></td></tr>
 <tr height="8px"></tr>
-<tr><td align="right">Autor:</td><td><input type="text" name="part-author" size="20" value="
-<?php if (!$valid and isset($_POST['author'])) { echo htmlspecialchars($_POST['author'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Autor:</td><td><input type="text" name="chapter-author" size="20" value="
+<?php if (!$valid and isset($_POST['chapter-author'])) { echo htmlspecialchars($_POST['chapter-author'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
-<tr><td align="right">Název:</td><td><input type="text" name="part-name" size="20" value="
-<?php if (!$valid and isset($_POST['name'])) { echo htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8'); } ?>
-" required></td></tr>
+<tr><td align="right">Název:</td><td><input type="text" name="chapter-name" size="20" value="
+<?php if (!$valid and isset($_POST['chapter-name'])) { echo htmlspecialchars($_POST['chapter-name'], ENT_QUOTES, 'UTF-8'); } ?>
+"></td></tr>
 <tr height="8px"></tr>
 <tr><td align="right"><u><b>Zdrojový dokument</b></u></td></td></tr>
 <tr height="8px"></tr>
-<tr><td align="right">Autor:</td><td><input type="text" name="part-source-author" size="20" value="
-<?php if (!$valid and isset($_POST['author'])) { echo htmlspecialchars($_POST['author'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Autor:</td><td><input type="text" name="chapter-src-author" size="20" value="
+<?php if (!$valid and isset($_POST['chapter-src-author'])) { echo htmlspecialchars($_POST['chapter-src-author'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
-<tr><td align="right">Název:</td><td><input type="text" name="part-source-name" size="20" value="
-<?php if (!$valid and isset($_POST['name'])) { echo htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8'); } ?>
-" required></td></tr>
-<tr><td align="right">Místo:</td><td><input type="text" name="part-source-place" size="20" value="
-<?php if (!$valid and isset($_POST['place'])) { echo htmlspecialchars($_POST['place'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Název:</td><td><input type="text" name="chapter-src-name" size="20" value="
+<?php if (!$valid and isset($_POST['chapter-src-name'])) { echo htmlspecialchars($_POST['chapter-src-name'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
-<tr><td align="right">Nakladatelství:</td><td><input type="text" name="part-source-publisher" size="20" value="
-<?php if (!$valid and isset($_POST['publisher'])) { echo htmlspecialchars($_POST['publisher'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Místo:</td><td><input type="text" name="chapter-src-place" size="20" value="
+<?php if (!$valid and isset($_POST['chapter-src-place'])) { echo htmlspecialchars($_POST['chapter-src-place'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
-<tr><td align="right">Rok:</td><td><input type="text" name="part-source-year" size="4" value="
-<?php if (!$valid and isset($_POST['year'])) { echo htmlspecialchars($_POST['year'], ENT_QUOTES, 'UTF-8'); } ?>
+<tr><td align="right">Nakladatelství:</td><td><input type="text" name="chapter-src-publisher" size="20" value="
+<?php if (!$valid and isset($_POST['chapter-src-publisher'])) { echo htmlspecialchars($_POST['chapter-src-publisher'], ENT_QUOTES, 'UTF-8'); } ?>
+"></td></tr>
+<tr><td align="right">Rok:</td><td><input type="text" name="chapter-src-year" size="4" value="
+<?php if (!$valid and isset($_POST['chapter-src-year'])) { echo htmlspecialchars($_POST['chapter-src-year'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
 </table>
 </div>
@@ -134,19 +144,19 @@ if ($valid) {
 <tr><td width="175" align="right"><u><b>Základní údaje</b></u></td></tr>
 <tr height="8px"></tr>
 <tr><td align="right">Autor:</td><td><input type="text" name="book-author" size="20" value="
-<?php if (!$valid and isset($_POST['author'])) { echo htmlspecialchars($_POST['author'], ENT_QUOTES, 'UTF-8'); } ?>
+<?php if (!$valid and isset($_POST['book-author'])) { echo htmlspecialchars($_POST['book-author'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
 <tr><td align="right">Název:</td><td><input type="text" name="book-name" size="20" value="
-<?php if (!$valid and isset($_POST['name'])) { echo htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8'); } ?>
-" required></td></tr>
+<?php if (!$valid and isset($_POST['book-name'])) { echo htmlspecialchars($_POST['book-name'], ENT_QUOTES, 'UTF-8'); } ?>
+"></td></tr>
 <tr><td align="right">Místo:</td><td><input type="text" name="book-place" size="20" value="
-<?php if (!$valid and isset($_POST['place'])) { echo htmlspecialchars($_POST['place'], ENT_QUOTES, 'UTF-8'); } ?>
+<?php if (!$valid and isset($_POST['book-place'])) { echo htmlspecialchars($_POST['book-place'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
 <tr><td align="right">Nakladatelství:</td><td><input type="text" name="book-publisher" size="20" value="
-<?php if (!$valid and isset($_POST['publisher'])) { echo htmlspecialchars($_POST['publisher'], ENT_QUOTES, 'UTF-8'); } ?>
+<?php if (!$valid and isset($_POST['book-publisher'])) { echo htmlspecialchars($_POST['book-publisher'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
 <tr><td align="right">Rok:</td><td><input type="text" name="book-year" size="4" value="
-<?php if (!$valid and isset($_POST['year'])) { echo htmlspecialchars($_POST['year'], ENT_QUOTES, 'UTF-8'); } ?>
+<?php if (!$valid and isset($_POST['book-year'])) { echo htmlspecialchars($_POST['book-year'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
 </table>
 </div>
@@ -155,7 +165,7 @@ if ($valid) {
 <tr height="8px"></tr>
 <tr><td width="175" align="right"><u><b>Ostatní</b></u></td></td></tr>
 <tr height="8px"></tr>
-<tr><td align="right">Poznámka:</td><td><input type="text" name="nook-note" size="30" value="
+<tr><td align="right">Poznámka:</td><td><input type="text" name="note" size="30" value="
 <?php if (!$valid and isset($_POST['quote'])) { echo htmlspecialchars($_POST['note'], ENT_QUOTES, 'UTF-8'); } ?>
 "></td></tr>
 <tr><td align="right">Odkaz:</td><td><input type="text" name="link" size="30" value="
