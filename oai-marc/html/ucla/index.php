ab7c29a51ddc2850f6051d0a561e7d7b0f9bd017
KyomaHooin <kyomahooin@gmail.com>
2021-10-03 23:48:16 +0200

    Minor fixes.

--- oai-marc/html/ucla/index.php
+++ oai-marc/html/ucla/index.php
@@ -46,7 +46,7 @@ $no_seven = 0;
 $seven = 0;
 
 if (!empty($tags)) {
-	echo '<p><u>Poslední záznam</u>: 22.03.2021</p>';
+	echo '<p><u>Poslední záznam</u>: 01.10.2021</p>';
 	echo "<table width='500' style='border-collapse: collapse;' border='1px'>"
 	. "<td></td><td colspan='4' align='center'><b>Podpole 7</b></td>"
 	. "<td colspan='4' align='center'><b>Bez podpole 7</b></td></tr>";
