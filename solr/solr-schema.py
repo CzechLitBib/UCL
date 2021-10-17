c96b3b1043b192a739e73a4e6e9827339e47c63d
KyomaHooin <kyomahooin@gmail.com>
2021-10-17 21:47:27 +0200

    Solr API too update.

--- solr/solr-schema.py
+++ solr/solr-schema.py
@@ -3,36 +3,62 @@
 # Solr API v2 schema tool
 #
 
-import requests,json
+import argparse,requests,json,sys
 
 URL='http://localhost:8983/solr/core/schema'
+
+# ARGS
+
+parser = argparse.ArgumentParser(description='Solr V2 API tool.')
+required = parser.add_argument_group('API')
+required.add_argument('--add', help='Add fields.', action='store_true')
+required.add_argument('--delete', help='Delete fields.', dest='field')
+required.add_argument('--list', help='List fields.', action='store_true')
+args = parser.parse_args()
+
+if not (args.add or args.field or args.list):
+	parser.error('Argument is required.')
+
+# MAIN
 
 session = requests.Session()
 session.headers.update({'Content-type':'application/json'})
 
-# Add Field
+if args.add:# Add Fields
+	field = {'add-field':{'name':'', 'type':''}}# indexed / stored / uninvertible (default true)
 
-field = {'add-field':{'name':'', 'type':''}}# indexed + stored + uninvertible: Default = True
+	field['add-field']['name'] = '245'
+	field['add-field']['type'] = 'strings'# string | strings (mv)
 
-field['add-field']['name'] = 'tag_245'
-field['add-field']['type'] = 'strings'
+	resp = session.post(URL, data=json.dumps(field))
 
-resp = session.post(URL, data=json.dumps(field))
+	if resp and resp.status_code == 200:
+		print("ok")
+	else:
+		print(resp.text)
 
-if resp and resp.status_code == 200:
-	print("ok")
-else:
-	print(resp.status_code)
-	print(resp.text)
+if args.field:# Delete Fields
+	field = {'delete-field':{'name':''}}
 
-# List Fields
+	field['delete-field']['name'] = args.field
 
-resp = session.get(URL + '/fields')
+	resp = session.post(URL, data=json.dumps(field))
 
-if resp and resp.status_code == 200:
-	data = json.loads(resp.text)
-	for F in data['fields']:
-		print(F['name'])
-else:
-	print('REquest failed.')
+	if resp and resp.status_code == 200:
+		print("ok")
+	else:
+		print(resp.text)
+
+if args.list:# List Fields
+	resp = session.get(URL + '/fields')
+
+	if resp and resp.status_code == 200:
+		data = json.loads(resp.text)
+		for F in data['fields']:
+			print(F['name'])
+	else:
+		print(resp.text)
+
+session.close()
+sys.exit(0)
 
