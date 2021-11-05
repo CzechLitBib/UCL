c6934c59cfde6b9680ffbc1d66f6643cf9654875
KyomaHooin <kyomahooin@gmail.com>
2021-11-05 13:35:02 +0100

    Kramerius update.

--- kramerius/kramerius.py
+++ kramerius/kramerius.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 #
 # kramerius5.nkp.cz
 # digitalniknihovna.cz/mzk
 # kramerius.lib.cas.cz
 #
-# Kramerius grab & link
+# https://registr.digitalniknihovna.cz/
 #
-
-from __future__ import print_function
-
-import requests,json,time
-
 # 0009-0468 Česká literatura
+#
 # Ceska literatura uuid:f9f595d7-4116-11e1-99e8-005056a60003
 #
 # [ {
@@ -35,15 +30,46 @@ import requests,json,time
 # ]
 #
 
-CESLIT='uuid:f9f595d7-4116-11e1-99e8-005056a60003'
+import requests,json,time,sys
 
-DATA=[]
+#print(json.dumps(resp, indent=4))
+
+KRAMERIUS,ISSN,ROOT=[],[],{}
+
+with open('issn.txt', 'r') as f: ISSN = f.read().splitlines()
+with open('kramerius.txt', 'r') as f: KRAMERIUS = f.read().splitlines()
+
+# PRE-SEARCH
+for I in ISSN:
+#for I in ['0009-0468']:
+	if '#' in I: continue# TODO: split milti-issn
+	for K in KRAMERIUS:
+		session = requests.Session()
+		try:
+			req = session.get(K + 'api/v5.0/search?q=issn:' + I + '&wt=json')
+			if req.status_code == 200:
+				resp = json.loads(req.text)
+				if resp['response']['numFound'] > 0:
+					for doc in resp['response']['docs']:
+						if doc['dostupnost'] == 'public':
+							if I not in ROOT: ROOT[I]={}
+							if K not in ROOT[I]: ROOT[I][K]=[]
+							if doc['root_pid'] not in ROOT[I][K]:
+								ROOT[I][K].append(doc['root_pid'])
+		except:
+			pass# SSL error.
+
+with open('root.txt', 'w') as f: f.write(json.dumps(ROOT))
+
+sys.exit(1)
+
+# LOOP
 VOLUME_INDEX=0
 ISSUE_INDEX=0
 
 session = requests.Session()
 
-req = session.get('https://kramerius.lib.cas.cz/search/api/v5.0/item/' + CESLIT + '/children')
+req = session.get('/item/UUID/children')
 if req.status_code == 200:
 	# VOLUUME
 	for volume in json.loads(req.text, strict=False):
@@ -56,8 +82,8 @@ if req.status_code == 200:
 				'volume_pid':volume_pid,
 				'issue':[]
 		})
-		print('volume: ' + volume_pid)
-		req = session.get('https://kramerius.lib.cas.cz/search/api/v5.0/item/' + volume_pid + '/children')
+		#print('volume: ' + volume_year)
+		req = session.get(URL + volume_pid + '/children')
 		if req.status_code == 200:
 			# ISSUE
 			for issue in json.loads(req.text, strict=False):
@@ -69,14 +95,15 @@ if req.status_code == 200:
 					'issue_pid':issue_pid,
 					'page':{}
 				})
-				print('   issue: ' + issue_pid)
-				req = session.get('https://kramerius.lib.cas.cz/search/api/v5.0/item/' + issue_pid + '/children')
+				#print('   issue: ' + issue_date)
+				req = session.get(URL + issue_pid + '/children')
 				if req.status_code == 200:
 					# PAGE
 					for page in json.loads(req.text, strict=False):
 						page_name = page['title']
 						page_pid = page['pid']
 						DATA[VOLUME_INDEX]['issue'][ISSUE_INDEX]['page'][page_name] = page_pid
+						#print('         page: ' + page_name)
 				# update indexes
 				ISSUE_INDEX+=1
 		# update /reset indexes
