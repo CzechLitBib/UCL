0dc775aad565fd11d49b82cabe33061dce5b926f
KyomaHooin <kyomahooin@gmail.com>
2021-11-07 21:55:42 +0100

    Kramerius 5.

--- kramerius/kramerius-linker.py
+++ kramerius/kramerius-linker.py
@@ -1,75 +1,77 @@
 #!/usr/bin/python3
 #
+# Kramerius UUID to Aleph 856 linker
+#
 
 import json,sys,re
 
-KRAMERIUS='ceslit.json'
-BIB='ceslit.bib'
-OUT='ceslit.link'
+IN='ucla.csv'
+OUT='ucla.aleph'
 
-URL='https://kramerius.lib.cas.cz/uuid/'
+MAP='map.json'
+
+def parse_Q(sysno,issn,q):
+
+	# REGEXP MAP BY ISSN
+	year = re.sub('^g[Rr]oč\. \d+, (\d+), .*$', '\\1', g).strip()
+	page = re.sub('q\d+:(\d+|\d+\/\d+)<(\d+)', '\\2', q).strip()
+
+	# ....
+	# ....
+
+	# COMPARATOR BY MAP
+
+	for volume in range(0, len (db)):
+		if year == MAP[issn][volume]['volume_year'] and int(Y) < 2011:
+			for issue in range(0, len(db[volume]['issue'])):
+				for page in db[volume]['issue'][issue]['page']:
+					if page == S:
+						url = MAP[issn].. + db[volume]['issue'][issue]['page'][page]
+						return sysno + ' 85641 L $$u' + url + u'$$yKramerius' + '$$4N'
+	return ''
+
+def parse_G(sysno,issn,g):
+
+	# REGEXP MAP BY ISSN
+	year = re.sub('^g[Rr]oč\. \d+, (\d+), .*$', '\\1', g).strip()
+	page = re.sub('^.*, s\. (\d+|\d+-\d+|\d+\/\d+)$', '\\1', g).split('-')[0].split('/')[0].strip()
+
+	# ....
+	# ....
+
+	# COMPARATOR BY MAP
+
+	for volume in range(0, len (db)):
+		if Y == db[volume]['volume_year'] and int(Y) < 2011:
+			for issue in range(0, len(db[volume]['issue'])):
+				for page in db[volume]['issue'][issue]['page']:
+					if page == S:
+						url = MAP[issn] + db[volume]['issue'][issue]['page'][page]
+						 return sysno + ' 85641 L $$u' + URL + u'$$yKramerius' + '$$4N'
+	return ''
+
+# MAIN
+
+with open(MAP, 'r') as f: MAP = json.loads(f.read())
+
+aleph = open(OUT,'w')
 
 TOTAL=0
 MATCH=0
 
-# ------------------------
-
-def convert_q(q,g,i,out,db):
-	Y =  re.sub('^g[Rr]oč\. \d+, (\d+), .*$', '\\1', g).strip()
-	S = re.sub('q\d+:(\d+|\d+\/\d+)<(\d+)', '\\2', q).strip()
-
-	for volume in range(0, len (db)):
-		if Y == db[volume]['volume_year'] and int(Y) < 2011:
-			for issue in range(0, len(db[volume]['issue'])):
-				for page in db[volume]['issue'][issue]['page']:
-					if page == S:
-						url = URL + db[volume]['issue'][issue]['page'][page]
-						out.write(i + ' 85641 L $$u' + url + u'$$yKramerius' + '$$4N\n')
-
-def convert_month(g,i,out,db):
-	Y = re.sub('^g[Rr]oč\. \d+, (\d+), .*$', '\\1', g).strip()
-	part = re.sub('^.*, s\. (\d+|\d+-\d+|\d+\/\d+)$', '\\1', g)
-	S = part.split('-')[0].split('/')[0].strip()
-
-	for volume in range(0, len (db)):
-		if Y == db[volume]['volume_year'] and int(Y) < 2011:
-			for issue in range(0, len(db[volume]['issue'])):
-				for page in db[volume]['issue'][issue]['page']:
-					if page == S:
-						url = URL + db[volume]['issue'][issue]['page'][page]
-						out.write(i + ' 85641 L $$u' + url + u'$$yKramerius' + '$$4N\n')
-
-# ------------------------
-
-out = open(OUT,'w')
-
-with open(KRAMERIUS, 'r') as k:
-	db = json.loads(k.read())
-
-with open(BIB,'r') as b:
-	for line in b:
-		TOTAL+=1
-		field = line.strip().split(chr(0x1F))
-		ID = field[0].strip()
-		G = field[4].strip()
-		Q = field[5].strip()
-		if re.match('q\d+:(\d+|\d+\/\d+)<\d+', Q):
-			MATCH+=1
-			convert_q(Q,G,ID,out,db)
-			continue
-		# MONTH LITERAL
-		if re.match('^g[Rr]oč\. \d+, \d+, č\. (\d+|\d+\/\d+), \D+, s\. (\d+|\d+-\d+|\d+\/\d+)$', G):
-			MONTH+=1
-			convert_month(G,ID,out,db)
-			continue
-		# MONTH NUMERIC
-		if re.match('^g[Rr]oč\. \d+, \d+, č\. (\d+|\d+\/\d+), \d+\. \d+\., s\. (\d+|\d+-\d+|\d+\/\d+)$', G):
-			MONTH+=1
-			convert_month(G,ID,out,db)
-			continue
-
-print('TOTAL: ' + str(TOTAL))
-print('MATCH: ' + str(MATCH))
-
-out.close()
+with open(IN,'r') as f:
+	for line in f:
+		data = line.split('$')
+		if line[5] in MAP:# issn
+			buff=''
+			# prase Q
+			if re.match('q\d+:(\d+|\d+\/\d+)<\d+', data[3]):
+				buff = parse_Q(data[0], data[5], data[3])
+			# prase G
+			elif re.match('^g[Rr]oč\. \d+, \d+, č\. (\d+|\d+\/\d+), \D+, s\. (\d+|\d+-\d+|\d+\/\d+)$', data[4]):
+			#elif re.match('^g[Rr]oč\. \d+, \d+, č\. (\d+|\d+\/\d+), \d+\. \d+\., s\. (\d+|\d+-\d+|\d+\/\d+)$', G):
+				buff = parse_G(data[0], data[5], data[3])
+			
+			if buff: aleph.write(buff + '\n')
+aleph.close()
 
