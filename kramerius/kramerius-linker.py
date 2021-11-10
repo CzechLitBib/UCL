2c551900a393b251a49ec836c80d7ec16cc8a023
KyomaHooin <kyomahooin@gmail.com>
2021-11-10 12:51:33 +0100

    Linker fix.

--- kramerius/kramerius-linker.py
+++ kramerius/kramerius-linker.py
@@ -12,20 +12,15 @@ OUT='ucla.aleph'
 ALL=0
 MATCH=0
 
-def linker(ID,ISSN,Y,R,C,S):
-
-	# COMPARATOR BY MAP
-
-	for volume in range(0, len (db)):
-		if year == MAP[issn][volume]['volume_year'] and int(Y) < 2011:
-			for issue in range(0, len(db[volume]['issue'])):
-				for page in db[volume]['issue'][issue]['page']:
-					if page == S:
-						url = MAP[issn] + db[volume]['issue'] + [issue]['page'][page]
-						return sysno + ' 85641 L $$u' + url + u'$$yKramerius' + '$$4N'
-	return ''
-
-
+#def linker(ID,ISSN,Y,R,C,S):
+#	for volume in range(0, len (db)):
+#		if year == MAP[issn][volume]['volume_year'] and int(Y) < 2011:
+#			for issue in range(0, len(db[volume]['issue'])):
+#				for page in db[volume]['issue'][issue]['page']:
+#					if page == S:
+#						url = MAP[issn] + db[volume]['issue'] + [issue]['page'][page]
+#						return sysno + ' 85641 L $$u' + url + u'$$yKramerius' + '$$4N'
+#	return ''
 
 with open(MAP, 'r') as f: MAP = json.loads(f.read())
 with open(IN,'r') as f: data = json.loads(f.read())
@@ -52,18 +47,17 @@ for rec in data['response']['docs']:
 	elif Q and re.match('^\d+:(\d+|\d+\/\d+)$', Q):
 		R = re.sub('(\d+):(\d+|\d+\/\d+)', '\\1',Q)
 		C = re.sub('(\d+):(\d+|\d+\/\d+)', '\\2',Q)
-	if G and not re.match('^Roč\. \d+, \d+, č\. (\d+|\d+\/\d+), \d+\. \d+\., s\. (\d+|\d+-\d+|\d+\/\d+)$', G):
+	if G and re.match('^Roč\. \d+, \d+, č\. (\d+|\d+\/\d+), \d+\. \d+\., s\. (\d+|\d+-\d+|\d+\/\d+)$', G):
 		Y = re.sub('^Roč\. \d+, (\d+),.*', '\\1', G)
 
 	if Y and R and C and S:
-		
-		#MATCH+=1
+		MATCH+=1
 	elif Y and R and C:
-		#MATCH+=1
-	#ALL+=1
+		MATCH+=1
+	ALL+=1
 
-#print(MATCH)
-#print(ALL)
+print(MATCH)
+print(ALL)
 
 #aleph.close()
 
