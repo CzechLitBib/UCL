362b13fc9137b01f97dd496b4b37d1e7c46796c7
KyomaHooin <kyomahooin@gmail.com>
2021-11-11 16:41:41 +0100

    Update 1.

--- kramerius/kramerius-solr.py
+++ kramerius/kramerius-solr.py
@@ -4,17 +4,14 @@
 #
 # model_path: /periodical/periodicalvolume/periodicalitem/page
 #
-#try: req = session.get(KRAMERIUS + 'api/v5.0/item/' + resp['response']['docs'][0]['parent_pid'][0])
 
 import requests,json,re,sys
 
 IN='in.json'
-ROOT={}
+ROOT='root.json'
 
 MATCH=0
 ALL=0
-
-# DEF
 
 def solr(G, ID, ISSN, Y, R, C, S=None):
 	for KRAMERIUS in MAP[ISSN]:
@@ -36,35 +33,47 @@ def solr(G, ID, ISSN, Y, R, C, S=None):
 				print(Y + ' ' + R + ' ' + C + ' ' + S)
 				for PAGE in resp['response']['docs']:
 					PAGE_PID = PAGE['PID']
-					query = 'PID:' + PAGE['parent_pid'][0].replace('-','\-').replace(':','\:')
-					try: req = session.get(KRAMERIUS + 'api/v5.0/search?q=' + query + '&fl=PID,title,details&rows=1&wt=json')
+					#query = 'PID:' + PAGE['parent_pid'][0].replace('-','\-').replace(':','\:')
+					#try: req = session.get(KRAMERIUS + 'api/v5.0/search?q=' + query + '&fl=PID,title,details&rows=1&wt=json')
+					#except:
+					#	print('Connection error.')
+					#	continue
+					#resp = json.loads(req.text)
+					#if re.match('^.*##' + C + '$' , resp['response']['docs'][0]['details'][0]):
+					#	URL = (
+					#		KRAMERIUS.replace('search', 'view') +
+					#		resp['response']['docs'][0]['PID'] + '?page=' +
+					#		PAGE_PID
+					#	)
+					#	return URL
+					
+					try: req = session.get(KRAMERIUS + 'api/v5.0/search/item/' + PAGE['parent_pid'])
 					except:
 						print('Connection error.')
 						continue
 					resp = json.loads(req.text)
-					if re.match('^.*##' + C + '$' , resp['response']['docs'][0]['details'][0]):
+					if C = resp['details'][partNumber]):
 						URL = (
 							KRAMERIUS.replace('search', 'view') +
-							resp['response']['docs'][0]['PID'] + '?page=' +
+							resp['response']['pid'] + '?page=' +
 							PAGE_PID
 						)
-						print(URL)
-			sys.exit(1)
+						return URL
 		session.close()
-
-# MAIN
+		sys.exit(1)
 
 with open(IN,'r') as f: DATA = json.loads(f.read())
-with open('root.json', 'r') as f: MAP = json.loads(f.read())
+with open(ROOT, 'r') as f: MAP = json.loads(f.read())
 
 for rec in DATA['response']['docs']:
 
-	ID,G,Q = rec['id'],'',''
+	ID,G,Q = '','',''
 
 	ISSN = rec['subfield_773-x'][0] 
 
 	if ISSN not in MAP: continue
 
+	ID = rec['id']
 	if 'subfield_773-g' in rec: G = rec['subfield_773-g'][0] 
 	if 'subfield_773-q' in rec: Q = rec['subfield_773-q'][0] 
 
@@ -83,12 +92,10 @@ for rec in DATA['response']['docs']:
 	if Y and R and C and S:
 		LINK = solr(G, ID, ISSN, Y, R, C, S)
 		if LINK:
-			print(LINK)
 			MATCH+=1
 	elif Y and R and C:
 		LINK = solr(G, ID, ISSN, Y, R, C)
 		if LINK:
-			print(LINK)
 			MATCH+=1
 	ALL+=1
 
