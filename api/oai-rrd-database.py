059842fdd3e7642aae0aaf9e6bc3ed7648ce1969
KyomaHooin <kyomahooin@gmail.com>
2022-01-20 16:40:58 +0100

    Simple RRD update.

--- api/oai-rrd-database.py
+++ api/oai-rrd-database.py
@@ -5,9 +5,7 @@ import rrdtool
 DATABASE='vufind.rrd'
 
 rrdtool.create(DATABASE,
-	'--step', '1h', '--start', 'now',
-	'DS:Aleph:GAUGE:4200:0:U',	# Data Source hearbeat/min/max
-	'RRA:LAST:0.5:1:2w',		# "Roud Robin Archive"
-	'RRA:LAST:0.5:1:2M',
-	'RRA:LAST:0.5:1:2y'
+	'--step', '3600', '--start', 'now',
+	'DS:aleph:GAUGE:7200:0:U',	# Data Source hearbeat/min/max
+	'RRA:LAST:0.5:1:336',		# "Roud Robin Archive"
 )
