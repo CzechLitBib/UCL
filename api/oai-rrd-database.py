d0ff75298711833a0bd57fbf2ffef5e7d7fe1de7
KyomaHooin <kyomahooin@gmail.com>
2022-01-20 13:51:24 +0100

    RRD resolution.

--- api/oai-rrd-database.py
+++ api/oai-rrd-database.py
@@ -5,18 +5,9 @@ import rrdtool
 DATABASE='vufind.rrd'
 
 rrdtool.create(DATABASE,
-	"--step", "3600", "--start", "now",	# 1hour update from now
-	"DS:Aleph:GAUGE:4200:0:U",		# Data Source 10min delay heartbeat, min/max 0/undef.
-	"RRA:LAST:0.5:1:288",			# 1-day "Roud Robin Archive"
-	"RRA:LAST:0.5:3:672",			# 1-week
-	"RRA:LAST:0.5:12:744",			# 1-month
-	"RRA:LAST:0.5:72:1480",			# 1-year
-	"RRA:MAX:0.5:1:288",
-	"RRA:MAX:0.5:3:672",
-	"RRA:MAX:0.5:12:744",
-	"RRA:MAX:0.5:72:1480",
-	"RRA:AVERAGE:0.5:1:288",
-	"RRA:AVERAGE:0.5:3:672",
-	"RRA:AVERAGE:0.5:12:744",
-	"RRA:AVERAGE:0.5:72:1480"
+	'--step', '1h', '--start', 'now',
+	'DS:Aleph:GAUGE:4200:0:U',	# Data Source hearbeat/min/max
+	'RRA:LAST:0.5:1:2w',		# "Roud Robin Archive"
+	'RRA:LAST:0.5:1:2M',
+	'RRA:LAST:0.5:1:2y'
 )
