cfe2dc3b4e2a66cf7147a8302f430d9b7b1bae53
KyomaHooin <kyomahooin@gmail.com>
2022-01-24 16:30:54 +0100

    Custom mod.

---
+++ vufind/module/SolrMarcUCL/config/module.config.php
@@ -0,0 +1,28 @@
+<?php
+
+return array (
+  'vufind' => 
+  array (
+    'plugin_managers' => 
+    array (
+      'recorddriver' => 
+      array (
+        'factories' => 
+        array (
+          'SolrMarcUCL\\RecordDriver\\SolrMarc' => 'VuFind\\RecordDriver\\SolrDefaultFactory',
+        ),
+        'aliases' => 
+        array (
+          'VuFind\\RecordDriver\\SolrMarc' => 'SolrMarcUCL\\RecordDriver\\SolrMarc',
+        ),
+        'delegators' => 
+        array (
+          'SolrMarcUCL\\RecordDriver\\SolrMarc' => 
+          array (
+            0 => 'VuFind\\RecordDriver\\IlsAwareDelegatorFactory',
+          ),
+        ),
+      ),
+    ),
+  ),
+);
