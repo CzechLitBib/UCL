598c1b7d86512403ce315194022856798aa43cb4
KyomaHooin <kyomahooin@gmail.com>
2022-01-28 12:41:20 +0100

    Theme update.

--- vufind/theme/CLB/theme.config.php
+++ vufind/theme/CLB/theme.config.php
@@ -2,5 +2,5 @@
 return [
     'extends' => 'bootstrap3',
     'favicon' => 'clb-favicon.ico',
- //  'helpers' => ['factories' => ['VuFind\View\Helper\Root\RecordDataFormatter' => 'CLB\View\Helper\Root\RecordDataFormatterFactory']]
+    'helpers' => ['factories' => ['VuFind\View\Helper\Root\RecordDataFormatter' => 'CLB\View\Helper\Root\RecordDataFormatterFactory']]
 ];
