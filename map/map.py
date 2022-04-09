e9a905e25c5935344d57fd4aaf043e5ff6f9d66a
KyomaHooin <kyomahooin@gmail.com>
2022-04-09 18:51:31 +0200

    Routing, JS init.

--- map/map.py
+++ map/map.py
@@ -1,28 +1,36 @@
 #!/bin/python3
-
+#
+# VuFind Map module app
+#
 import json
 
-from flask import Flask,render_template
-
-#from wtforms import Form, StringField, SubmitField
-#from wtforms.validators import DataRequired
+from flask import Flask,render_template,make_response
 
 # INIT
 
 app = Flask(__name__)
 
-# DEF / ROUTE
-
-@app.route('/map')
+@app.route('/map')# ROOT
 def index():
 	return render_template('index.html')
 
-@app.route('/map/json')
-def db():
+@app.route('/map/q')# QUERY
+def query():
+	# attrib parsing..
+	# JOSN | SQLITE | SOLR query
+	# return JSON location + markers coords..
 	data = []
-	return json.dumps(data)
+	return render_template('index.html')
+
+@app.route('/map/data')# DATA
+def data():
+	data = {'country': ('lat','lon')}
+	headers = {'Content-Type': 'application/json'}
+	# return JSON location + markers coords..
+	return make_response(json.dumps(data), 200, headers)
 
 # MAIN
+
 if __name__ == '__main__':
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
 	#app.run(debug=False)
--- map/static/map.js
+++ map/static/map.js
@@ -1,10 +1,17 @@
-//
-// Leaflet Map
-//
+/* Leaflet Map JS */
 
-var map = L.map('map').setView([49.817492,15.472962], 13);
+BASECOORDS = [49.817492,15.472962];
+
+var map = L.map('map').setView(BASECOORDS, 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+var layer = L.layerGroup();
+
+fetch('http://vufind2-dev.ucl.cas.cz/map/data')
+    .then(res => res.json())
+    .then((out) => {
+        console.log('Output: ', out);
+}).catch(err => console.error(err));
