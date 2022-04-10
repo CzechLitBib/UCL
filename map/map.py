c879e1b0f96671e3b28f024059ce93907db37921
KyomaHooin <kyomahooin@gmail.com>
2022-04-10 12:04:47 +0200

    Demo final.

--- map/map.py
+++ map/map.py
@@ -16,22 +16,11 @@ def index():
 
 @app.route('/map/q')# QUERY
 def query():
-	# attrib parsing..
-	# JOSN | SQLITE | SOLR query
-	# return JSON location + markers coords..
-	data = []
 	return render_template('index.html')
-
-@app.route('/map/data')# DATA
-def data():
-	data = {'country': ('lat','lon')}
-	headers = {'Content-Type': 'application/json'}
-	# return JSON location + markers coords..
-	return make_response(json.dumps(data), 200, headers)
 
 # MAIN
 
 if __name__ == '__main__':
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
-	#app.run(debug=False)
+	app.run(debug=False)
 	app.run()
--- map/static/map.js
+++ map/static/map.js
@@ -8,10 +8,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-var layer = L.layerGroup();
 
-fetch('http://vufind2-dev.ucl.cas.cz/map/data')
-    .then(res => res.json())
-    .then((out) => {
-        console.log('Output: ', out);
-}).catch(err => console.error(err));
+var marker =  L.marker([49.817492,15.472962]).addTo(map);
+
+marker.bindPopup('<b>Marker</b><br>Mapový modul pro <a target="_blank" href="http://vufind2-dev.ucl.cas.cz.">VuFind</a>.').openPopup();
+
