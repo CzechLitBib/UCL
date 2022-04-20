68b9ebb2bc7164d6eb5ab57dd0e9de1ac1110519
KyomaHooin <kyomahooin@gmail.com>
2022-04-20 15:42:02 +0200

    Vyvoj bootstrap login.

--- html/vyvoj/index.php
+++ html/vyvoj/index.php
@@ -1,92 +1,44 @@
-<?php
-
-session_start();
-
-# logged in redirect
-if (!empty($_SESSION['auth']) and isset($_SESSION['page'])) {
-	header('Location: ' . $_SESSION['page']);
-	exit();
-}
-
-$_SESSION['auth'] = False;
-$_SESSION['group'] = 'user';
-
-$admin = [];
-$nkp   = [];
-$form  = [];
-$solr  = [];
-
-if (!isset($_SESSION['error'])) { $_SESSION['error'] = False; }
-
-$authorized = False;
-
-if (isset($_POST['user']) and isset($_POST['pass'])) {
-	
-	$ldap_dn = 'xxx';
-	$ldap_conn = ldap_connect('xxx');
-
-	ldap_set_option($ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
-	ldap_set_option($ldap_conn, LDAP_OPT_REFERRALS, 0);
-	ldap_set_option($ldap_conn, LDAP_OPT_NETWORK_TIMEOUT, 10);
-
-	$ldap_bind = @ldap_bind($ldap_conn, $ldap_dn, $_POST['pass']);
-
-	if ($ldap_bind) { $authorized = True; }
-}
-?>
-
 <!doctype html>
-<html lang="en">
+<html lang="cs">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Vývoj - ČLB</title>
-	<link rel="stylesheet" type="text/css" href="/vyvoj/bootstrap.min.css"/>
-	<link rel="stylesheet" type="text/css" href="/vyvoj/signin.css"/>
-	<link rel="stylesheet" href="/vyvoj/bootstrap.min.js"/>
-	<link rel="icon" href="favicon.ico">
-</head>
-<body class="text-center">
-<main class="form-signin">
-<form action="." method="post">
-	<img class="mb-2" src="logo3.png" alt="Logo" width="209"/>
-	<h2 class="h3 mb-3 fw-normal">Přihlašte se prosím</h2>
+	<title>ČLB - Vývoj</title>
+	<link href="bootstrap.min.css" rel="stylesheet">
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" href="favicon/apple-touch-icon.png" sizes="180x180">
+	<link rel="icon" href="favicon/favicon-32x32.png" sizes="32x32" type="image/png">
+	<link rel="icon" href="favicon/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#7952b3">
+	<!-- Custom styles -->
 
-	<div class="form-floating">
-		<input type="text" class="form-control" id="floatingInput" name="user" placeholder="uživatelské jméno" required autofocus>
-		<label for="floatingInput">Uživatelské jméno</label>
+</head>
+
+<body class="bg-light text-center">
+<main class="w-100 m-auto" style="max-width:330px; padding-top:35px;">
+
+<img src="/vyvoj/logo.png" alt="ČLB logo" width="209"/>
+
+<h2 class="my-4">Přihlašte se prosím</h2>
+
+<form action="." method="post">
+	<div class="form-floating mt-4 mb-1">
+		<input type="text" class="form-control" id="user" name="user" required autofocus>
+		<label for="user">Uživatelské jméno</label>
 	</div>
-	<div class="form-floating">
-		<input type="password" class="form-control" id="floatingPassword" name="pass" placeholder="heslo" required>
-		<label for="floatingPassword">Heslo</label>
+	<div class="form-floating mb-4">
+		<input type="password" class="form-control" id="secret" name="pass" required>
+		<label for="pass">Heslo</label>
 	</div>
-	<button class="w-100 btn btn-lg btn-primary" type="submit">Přihlásit</button>
-	<p class="mt-3 text-muted">&copy; 2021–<?php echo date("Y"); ?></p>
+	<button class="btn btn-lg btn-primary w-100" type="submit">Přihlásit</button>
 </form>
 
-<?php
-
-if (isset($_POST['user']) and isset($_POST['pass'])) {
-	if (!$authorized) {
-		echo '<div class="alert alert-danger" role="alert">Přihlášení selhalo.</div></div>';
-	} else {
-		$_SESSION['auth'] = True;
-
-		if (in_array($_POST['name'], $admin)) { $_SESSION['group'] =  'admin'; }
-		if (in_array($_POST['name'], $form)) { $_SESSION['group'] =  'form'; }
-		if (in_array($_POST['name'], $solr)) { $_SESSION['group'] =  'solr'; }
-		if (in_array($_POST['name'], $nkp)) { $_SESSION['group'] =  'nkp'; }
-
-		if(empty($_SESSION['page'])) { $_SESSION['page'] = '/main/'; }// default page
-
-		header('Location: ' . $_SESSION['page']);
-		exit();
-	}	
-}
-
-?>
+<p class="my-4 text-muted">&copy; 2021–<?php echo date("Y"); ?></p>
 
 </main>
+
+<link rel="stylesheet" href="/vyvoj/bootstrap.min.js"/>
+
 </body>
 </html>
 
