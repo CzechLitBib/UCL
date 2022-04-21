1f1ff7050c3cfb5c193197936f0861c422923c5a
KyomaHooin <kyomahooin@gmail.com>
2022-04-21 21:00:18 +0200

    Web update main.

--- html/vyvoj/main/index.php
+++ html/vyvoj/main/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>ČLB - Vývoj</title>
+	<title>ČLB Vývoj</title>
 	<link href="../bootstrap.min.css" rel="stylesheet">
 	<!-- Favicons -->
 	<link rel="apple-touch-icon" href="../favicon/apple-touch-icon.png" sizes="180x180">
@@ -27,8 +27,9 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 			</ul>
-			<form class="d-flex">
-			<img class="d-inline-block align-text-center mx-2" src="../icons/person.svg" alt="User" width="32" height="32"> 
+			<span clas="navbar-text">Username</span>
+			<form class="d-flex align-items-center">
+			<img class="d-inline-block align-text-center mx-2" src="../icons/person-fill.svg" alt="User" width="32" height="32"> 
 			</form>
 		</div>
 	</div>
@@ -38,88 +39,112 @@
 <div class="container px-4 py-2" id="icon-grid">
 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
 
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="daily/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/calendar-date.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">Denní kontrola</h4>
+          <p>Kontrola záznamů Aleph protokolem OAI.</p>
         </div>
       </div>
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="weekly/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/calendar-week.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">Týdenní kontrola</h4>
+          <p>Kontrola záznamů Aleph protokolem OAI.</p>
         </div>
       </div>
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="seven/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/calendar-month.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">7</h4>
+          <p>Měsíční statistika podpole 7.</p>
         </div>
       </div>
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="nkp/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/calendar-month.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">NKP</h4>
+          <p>Měsíční statistika podpole 7 pro NKP.</p>
         </div>
       </div>
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="kat/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/calendar-month.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">CAT</h4>
+          <p>Měsíční statistika polí CAT/KAT.</p>
         </div>
       </div>
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="clo/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/file-earmark-binary.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">CLO</h4>
+          <p>Statistika podpole 7 pro set CLO.</p>
         </div>
       </div>
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="uclo/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/file-earmark-binary.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">UCLO</h4>
+          <p>Statistika podpole 7 pro set UCLO.</p>
         </div>
       </div>
-      <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+      <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="error/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/exclamation-triangle.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">Chybové kódy</h4>
+          <p>Popis kódů kontroly protokolem OAI.</p>
         </div>
       </div>
- <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+ <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="/form/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/arrow-up-right-square.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">Formulář</h4>
+          <p>Formulář pro zasílání navrhů dokumentů.</p>
         </div>
       </div>
- <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+ <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="form-data/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/folder.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">Formulář / Data</h4>
+          <p>Správa zaslaných návrhů vstupního formuláře.</p>
         </div>
       </div>
- <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+ <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="api/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/server.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">Vufind API</h4>
+          <p>Aktulaní stahování a popis rozhraní.</p>
         </div>
       </div>
- <div class="col d-flex align-items-start">
-	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/person.svg" alt="User" width="32" height="32"> 
+ <div class="col d-flex align-items-start position-relative">
+	<a class="stretched-link" href="aleph/">
+	<img class="bi text-muted flex-shrink-0 me-3" src="../icons/search.svg" alt="User" width="32" height="32"> 
+	</a>
         <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
+          <h4 class="fw-bold mb-0">Aleph Solr</h4>
+          <p>Rychlé vyhedávací rozhraní pro UCLO set.</p>
         </div>
       </div>
 
