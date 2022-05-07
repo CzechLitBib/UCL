6e55e61cd54d00eef6fc5901a3e7639e5e9fadc7
KyomaHooin <kyomahooin@gmail.com>
2022-05-07 18:03:15 +0200

    Typo.

--- html/aleph/index.php
+++ html/aleph/index.php
@@ -115,7 +115,7 @@ if (!empty($_POST)) {
 		<span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<a href="/logout.php" class="btn btn-sm mx-1 ms-auto btn-outline-light">Odhlasit</a>
+		<a href="/logout.php" class="btn btn-sm mx-1 ms-auto btn-outline-light">Odhlásit</a>
 		<span class="mx-2 align-middle"><b><?php echo $_SESSION['username'];?></b></span>
 		<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"><path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/><svg/>
 	</div>
