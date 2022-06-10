7fd3b2d280f453b3a31fd68c7e8c5d4efabcd8ab
KyomaHooin <kyomahooin@gmail.com>
2022-06-10 17:32:06 +0200

    Ajax 2.

--- vufind/export/static/custom.js
+++ vufind/export/static/custom.js
@@ -1,21 +1,53 @@
 
 // JS Fetch API
 
-const spinner = document.getElementById("spinner");
+const spinner = document.getElementById('spinner');
+const warn = document.getElementById('alert');
 
-async function check_status() {
-	return await fetch('/export?status',{
-		method:"GET",
+function date() {
+	var today = new Date();
+	return today.toISOString().substring(0, 10);
+}
+
+async function payload(format) {
+	return await fetch('/export', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'text/plain',
+		},
+		body: format
 	})
-	.then(response => response)
-	.then(data => {
-		console.log(data)
+	.then(response => {
+		if (!response.ok) {
+			throw new Error('Network error.');
+	}
+		 return response.blob();
+	})
+        .then(blob => {
+		var url = window.URL.createObjectURL(blob);
+		var a = document.createElement('a');
+		a.href = url;
+		a.download = 'vufind-' + date() + '.' + format;
+		document.body.appendChild(a);
+		a.click();
+		a.remove();
+		return true;
+        })
+	.catch(error => {
+		console.error(error);
+		return false;
 	});
 }
 
-async function progress() {
+async function download(format) {
+	warn.setAttribute('hidden', '');
 	spinner.removeAttribute('hidden');
-	const f = await this.check_status();
+
+	const download = await this.payload(format);
+
 	spinner.setAttribute('hidden', '');
-	console.log(f)
+	if (!download) {
+		warn.removeAttribute('hidden');
+	}
 }
+
