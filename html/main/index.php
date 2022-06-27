4ffcdca1209c36ede5e4c601c974227dbfe735f4
KyomaHooin <kyomahooin@gmail.com>
2022-06-27 20:23:54 +0200

    Combined UCLO update.

--- html/main/index.php
+++ html/main/index.php
@@ -95,7 +95,7 @@ if(empty($_SESSION['auth'])) {
 		<p>Měsíční statistika polí CAT/KAT.</p>
 	</div>
 </div>
-<div class="col d-flex align-items-start position-relative" onmouseover="menuselect('clo')" onmouseleave="menuleave('clo')">
+<!--<div class="col d-flex align-items-start position-relative" onmouseover="menuselect('clo')" onmouseleave="menuleave('clo')">
 	<a class="stretched-link" href="/clo/">
 	<svg xmlns="http://www.w3.org/2000/svg" id="clo" width="32" height="32" fill="currentColor" class="bi bi-file-earmark-binary flex-shrink-0 me-3" viewBox="0 0 16 16"><path d="M7.05 11.885c0 1.415-.548 2.206-1.524 2.206C4.548 14.09 4 13.3 4 11.885c0-1.412.548-2.203 1.526-2.203.976 0 1.524.79 1.524 2.203zm-1.524-1.612c-.542 0-.832.563-.832 1.612 0 .088.003.173.006.252l1.559-1.143c-.126-.474-.375-.72-.733-.72zm-.732 2.508c.126.472.372.718.732.718.54 0 .83-.563.83-1.614 0-.085-.003-.17-.006-.25l-1.556 1.146zm6.061.624V14h-3v-.595h1.181V10.5h-.05l-1.136.747v-.688l1.19-.786h.69v3.633h1.125z"/><path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/></svg>
 	</a>
@@ -103,7 +103,7 @@ if(empty($_SESSION['auth'])) {
 		<h4 class="fw-bold mb-0">CLO</h4>
 		<p>Statistika podpole 7 pro set CLO.</p>
 	</div>
-</div>
+</div>--!>
 <div class="col d-flex align-items-start position-relative" onmouseover="menuselect('uclo')" onmouseleave="menuleave('uclo')">
 	<a class="stretched-link" href="/uclo/">
 	<svg xmlns="http://www.w3.org/2000/svg" id="uclo" width="32" height="32" fill="currentColor" class="bi bi-file-earmark-binary flex-shrink-0 me-3" viewBox="0 0 16 16"><path d="M7.05 11.885c0 1.415-.548 2.206-1.524 2.206C4.548 14.09 4 13.3 4 11.885c0-1.412.548-2.203 1.526-2.203.976 0 1.524.79 1.524 2.203zm-1.524-1.612c-.542 0-.832.563-.832 1.612 0 .088.003.173.006.252l1.559-1.143c-.126-.474-.375-.72-.733-.72zm-.732 2.508c.126.472.372.718.732.718.54 0 .83-.563.83-1.614 0-.085-.003-.17-.006-.25l-1.556 1.146zm6.061.624V14h-3v-.595h1.181V10.5h-.05l-1.136.747v-.688l1.19-.786h.69v3.633h1.125z"/><path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/></svg>
