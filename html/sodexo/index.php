5fd77ab99b227a6df4dc89bd0489970ccfee8ca8
KyomaHooin <kyomahooin@gmail.com>
2022-06-29 17:17:16 +0200

    Sodexo init.

---
+++ html/sodexo/index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="cs">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sodexo Flexi Pass<?php echo date('Y');?></title>
+	<link href="bootstrap.min.css" rel="stylesheet">
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" href="./favicon/apple-touch-icon.png" sizes="180x180">
+	<link rel="icon" href="./favicon/favicon-32x32.png" sizes="32x32" type="image/png">
+	<link rel="icon" href="./favicon/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="mask-icon" href="./favicon/safari-pinned-tab.svg" color="#7952b3">
+	<!-- Custom styles -->
+	<link href="custom.css" rel="stylesheet">
+</head>
+
+<body class="bg-light">
+<main class="container text-center">
+<form method="post" action="." enctype="multipart/form-data">
+
+</form>
+</main>
+
+</body>
+</html>
+
