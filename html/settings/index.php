20ff58416edf080f09b491578e0573091b406c55
KyomaHooin <kyomahooin@gmail.com>
2022-08-21 12:55:18 +0200

    Dict init.

--- html/settings/index.php
+++ html/settings/index.php
@@ -446,6 +446,50 @@ if ($db) {
 </table>
 </form>
 
+<h3>Vyjímky</h3>
+
+<form method="post" action="." enctype="multipart/form-data">
+<table class="table my-4">
+	<thead>
+	<tr>
+		<th scope="col">SysNo</th>
+		<th scope="col">Kód</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+	<td class="align-middle">
+		<input class="form-control text-center" id="exception-sysno" name="exception-sysno" maxlength="9" type="text" value="<?php echo $ex_sysno;?>" size="8" list="exception-list">
+		<datalist id="exception-list">
+
+		</datalist>
+	</td>
+	<td class="align-middle"><input type="text" class="form-control" id="exception-list" size="47" name="exception-list" value="<?php echo $exception_list;?>"></td>
+	<td class="align-middle">
+		<div class="row-3 mb-3">
+			<input type="submit" id="exception-save" name="exception-save" value="exception-save" hidden>
+			<svg xmlns="http://www.w3.org/2000/svg" onclick="exception_on_save()" width="24" height="24" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg>	
+		</div>
+		<div class="row-3 mt-3">
+			<input type="submit" id="exception-delete" name="exception-delete" value="exception-delete" hidden>
+			<svg xmlns="http://www.w3.org/2000/svg" onclick="exception_on_delete()" width="24" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>
+		</div>
+	</td>
+	<td class="align-middle">
+		<div class="row-3 mb-3">
+			<input type="submit" id="exception-display" name="exception-display" value="exception-display" hidden>
+			<svg xmlns="http://www.w3.org/2000/svg" onclick="on_display('exception')" width="24" height="24" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/></svg>
+		</div>
+		<div class="row-3 mt-3">
+			<input type="submit" id="exception-export" name="exception-export" value="exception-export" hidden>
+			<svg xmlns="http://www.w3.org/2000/svg" onclick="on_export('exception')" width="24" height="24" fill="currentColor" class="bi bi-filetype-csv" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM3.517 14.841a1.13 1.13 0 0 0 .401.823c.13.108.289.192.478.252.19.061.411.091.665.091.338 0 .624-.053.859-.158.236-.105.416-.252.539-.44.125-.189.187-.408.187-.656 0-.224-.045-.41-.134-.56a1.001 1.001 0 0 0-.375-.357 2.027 2.027 0 0 0-.566-.21l-.621-.144a.97.97 0 0 1-.404-.176.37.37 0 0 1-.144-.299c0-.156.062-.284.185-.384.125-.101.296-.152.512-.152.143 0 .266.023.37.068a.624.624 0 0 1 .246.181.56.56 0 0 1 .12.258h.75a1.092 1.092 0 0 0-.2-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.551.05-.776.15-.225.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.122.524.082.149.2.27.352.367.152.095.332.167.539.213l.618.144c.207.049.361.113.463.193a.387.387 0 0 1 .152.326.505.505 0 0 1-.085.29.559.559 0 0 1-.255.193c-.111.047-.249.07-.413.07-.117 0-.223-.013-.32-.04a.838.838 0 0 1-.248-.115.578.578 0 0 1-.255-.384h-.765ZM.806 13.693c0-.248.034-.46.102-.633a.868.868 0 0 1 .302-.399.814.814 0 0 1 .475-.137c.15 0 .283.032.398.097a.7.7 0 0 1 .272.26.85.85 0 0 1 .12.381h.765v-.072a1.33 1.33 0 0 0-.466-.964 1.441 1.441 0 0 0-.489-.272 1.838 1.838 0 0 0-.606-.097c-.356 0-.66.074-.911.223-.25.148-.44.359-.572.632-.13.274-.196.6-.196.979v.498c0 .379.064.704.193.976.131.271.322.48.572.626.25.145.554.217.914.217.293 0 .554-.055.785-.164.23-.11.414-.26.55-.454a1.27 1.27 0 0 0 .226-.674v-.076h-.764a.799.799 0 0 1-.118.363.7.7 0 0 1-.272.25.874.874 0 0 1-.401.087.845.845 0 0 1-.478-.132.833.833 0 0 1-.299-.392 1.699 1.699 0 0 1-.102-.627v-.495Zm8.239 2.238h-.953l-1.338-3.999h.917l.896 3.138h.038l.888-3.138h.879l-1.327 4Z"/></svg>
+		</div>
+	</td>
+	</tr>
+</tbody>
+</table>
+</form>
+
 <h3>Recenze</h3>
 
 <?php
@@ -575,6 +619,66 @@ if ($db) {
 		<div class="row-3 mb-3">
 			<input type="submit" id="code-save" name="code-save" value="code-save" hidden>
 			<svg xmlns="http://www.w3.org/2000/svg" onclick="code_on_save()" width="24" height="24" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg>	
+		</div>
+		<div class="row-3 mt-3">
+			<input type="submit" id="code-export" name="code-export" value="code-export" hidden>
+			<svg xmlns="http://www.w3.org/2000/svg" onclick="on_export('code')" width="24" height="24" fill="currentColor" class="bi bi-filetype-csv" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM3.517 14.841a1.13 1.13 0 0 0 .401.823c.13.108.289.192.478.252.19.061.411.091.665.091.338 0 .624-.053.859-.158.236-.105.416-.252.539-.44.125-.189.187-.408.187-.656 0-.224-.045-.41-.134-.56a1.001 1.001 0 0 0-.375-.357 2.027 2.027 0 0 0-.566-.21l-.621-.144a.97.97 0 0 1-.404-.176.37.37 0 0 1-.144-.299c0-.156.062-.284.185-.384.125-.101.296-.152.512-.152.143 0 .266.023.37.068a.624.624 0 0 1 .246.181.56.56 0 0 1 .12.258h.75a1.092 1.092 0 0 0-.2-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.551.05-.776.15-.225.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.122.524.082.149.2.27.352.367.152.095.332.167.539.213l.618.144c.207.049.361.113.463.193a.387.387 0 0 1 .152.326.505.505 0 0 1-.085.29.559.559 0 0 1-.255.193c-.111.047-.249.07-.413.07-.117 0-.223-.013-.32-.04a.838.838 0 0 1-.248-.115.578.578 0 0 1-.255-.384h-.765ZM.806 13.693c0-.248.034-.46.102-.633a.868.868 0 0 1 .302-.399.814.814 0 0 1 .475-.137c.15 0 .283.032.398.097a.7.7 0 0 1 .272.26.85.85 0 0 1 .12.381h.765v-.072a1.33 1.33 0 0 0-.466-.964 1.441 1.441 0 0 0-.489-.272 1.838 1.838 0 0 0-.606-.097c-.356 0-.66.074-.911.223-.25.148-.44.359-.572.632-.13.274-.196.6-.196.979v.498c0 .379.064.704.193.976.131.271.322.48.572.626.25.145.554.217.914.217.293 0 .554-.055.785-.164.23-.11.414-.26.55-.454a1.27 1.27 0 0 0 .226-.674v-.076h-.764a.799.799 0 0 1-.118.363.7.7 0 0 1-.272.25.874.874 0 0 1-.401.087.845.845 0 0 1-.478-.132.833.833 0 0 1-.299-.392 1.699 1.699 0 0 1-.102-.627v-.495Zm8.239 2.238h-.953l-1.338-3.999h.917l.896 3.138h.038l.888-3.138h.879l-1.327 4Z"/></svg>
+		</div>
+	</td>
+
+	</tr>
+</tbody>
+</table>
+</form>
+
+<h3>Slovníky</h3>
+
+<form method="post" action="." enctype="multipart/form-data">
+<table class="table my-4">
+	<thead>
+	<tr>
+		<th scope="col">Pole</th>
+		<th scope="col">Hodnoty</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+	<td>
+		<div class="form-check">
+		<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+		<label class="form-check-label" for="exampleRadios1">260/264a</label>
+		</div>
+		<div class="form-check">
+		<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+		<label class="form-check-label" for="exampleRadios2">260/264b</label>
+		</div>
+		<div class="form-check">
+		<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+		<label class="form-check-label" for="exampleRadios3">490a</label>
+		</div>
+		<div class="form-check">
+		<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+		<label class="form-check-label" for="exampleRadios3">773t</label>
+		</div>
+		<div class="form-check">
+		<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+		<label class="form-check-label" for="exampleRadios3">336</label>
+		</div>
+		<div class="form-check">
+		<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+		<label class="form-check-label" for="exampleRadios3">337</label>
+		</div>
+		<div class="form-check">
+		<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+		<label class="form-check-label" for="exampleRadios3">338</label>
+		</div>
+	</td>
+	<td class="align-middle"><textarea class="form-control" id="dictionary-data" name="dictionary-data" rows="5">
+</textarea></td>
+	<td class="align-middle">
+		<div class="row-3 mb-3">
+			<input type="submit" id="code-save" name="code-save" value="code-save" hidden>
+			<svg xmlns="http://www.w3.org/2000/svg" onclick="dict_on_save()" width="24" height="24" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg>	
 		</div>
 		<div class="row-3 mt-3">
 			<input type="submit" id="code-export" name="code-export" value="code-export" hidden>
