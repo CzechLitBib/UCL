0519fd8aa2f007b237218401a07e840d56a74138
KyomaHooin <kyomahooin@gmail.com>
2022-09-22 13:26:15 +0200

    NBSP.

--- html/form/index.php
+++ html/form/index.php
@@ -155,7 +155,7 @@ if ($_SESSION['message'] > 0) {
 	<div class="accordion-item">
 		<h2 class="accordion-header" id="headingOne">
 			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-	Tento formulář slouží pro zasílání návrhů dokumentů ke zpracování pro potřeby databází České literární bibliografie a repozitáře ASEP Knihovny AV.
+	Tento formulář slouží pro zasílání návrhů dokumentů ke zpracování pro potřeby databází České literární bibliografie a&nbsp;repozitáře ASEP Knihovny AV.
 			</button>
 		</h2>
 		<div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
