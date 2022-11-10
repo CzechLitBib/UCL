fee057734d40636768a8f3be10ec1b5fafa490e4
KyomaHooin <kyomahooin@gmail.com>
2022-11-10 20:29:08 +0100

    Pro.

---
+++ history/history.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#
+# History of UCL cataloging
+#
+# apt-get install python3-matplotlib x11-common-
+#
+#  Y axis 008/00-05
+#  X axis 008/07-10
+#
+# This chart was implemented based on Benjamin Schmidt's blog post
+# http://sappingattention.blogspot.com/2017/05/a-brief-visual-history-of-marc.html (Tuesday, May 16, 2017).
+#
+
+import matplotlib
+
+matplotlib.use('Agg')# no display
+
+from matplotlib import pyplot as plt
+
+#figure = plt.figure()
+
+#plt.hist2d(x_arr, y_arr, bins=common_bin_arr)
+
+#figure.savefig(savepath)
+
