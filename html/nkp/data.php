99ad11fa253a665a40a031924ec92bc99a492371
KyomaHooin <kyomahooin@gmail.com>
2022-11-21 17:30:15 +0100

    Cleanup.

--- html/nkp/data.php
+++ html/nkp/data.php
@@ -39,7 +39,6 @@ if(!isset($_SESSION['nkp_year'])) { $_SESSION['nkp_year'] = Null; }
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>ČLB Vývoj - NKP</title>
-	<link href="../bootstrap.min.css" rel="stylesheet">
 	<!-- Favicons -->
 	<link rel="apple-touch-icon" href="../favicon/apple-touch-icon.png" sizes="180x180">
 	<link rel="icon" href="../favicon/favicon-32x32.png" sizes="32x32" type="image/png">
