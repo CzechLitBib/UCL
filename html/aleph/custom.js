2d3f1fa5ecb1dbb6498059c1cc98fb3fd2bd5284
KyomaHooin <kyomahooin@gmail.com>
2022-12-05 18:14:12 +0100

    Fix JS.

--- html/aleph/custom.js
+++ html/aleph/custom.js
@@ -1,4 +1,45 @@
 
+// add query
+function last_query_id() {
+	queries = document.getElementsByName('query[]');
+	last = queries[queries.length - 1].id;
+	id = last.match('\\d+')
+	if (Array.isArray(id) && id.length) {
+		return Number(id[0]) + 1
+	} else {
+		return null;
+	}
+}
+
+function add_query() {
+	id = String(last_query_id());
+	query = document.createDocumentFragment();
+
+	row = document.createElement('div');
+	row.className = 'row mt-2 gx-0 justify-content-center';
+	col = document.createElement('div');
+	col.className = 'col';
+	form = document.createElement('div');
+	form.className = 'form-floating';
+	input = document.createElement('input');
+	input.type = 'text';
+	input.className = 'form-control';
+	input.id = 'query' + id;
+	input.name = 'query[]';
+	input.value = '';
+	label = document.createElement('label');
+	label.htmlFor= 'query' + id;
+	label.textContent = 'Podmínka';
+
+	form.appendChild(input);
+	form.appendChild(label);
+	col.appendChild(form);
+	row.appendChild(col);
+	query.appendChild(row);
+
+	document.getElementById('query-group').appendChild(query);
+}
+
 // modal
 modal = new bootstrap.Modal(document.getElementById('help'));
 function help() {
@@ -21,9 +62,9 @@ function get_selection(core) {
 		checks[i].disabled = false;
 		if (core == 'clo') {
 			if (
-				!clo.includes(checks[i].name.replace("field_", "")) &&
-				!clo.includes(checks[i].name.replace(/subfield_(.+)-./, "$1")) &&
-				!clo.includes(checks[i].name.replace(/local_(LDR|008).+/, "$1"))
+				!clo.includes(checks[i].value.replace("field_", "")) &&
+				!clo.includes(checks[i].value.replace(/subfield_(.+)-./, "$1")) &&
+				!clo.includes(checks[i].value.replace(/local_(LDR|008).+/, "$1"))
 			) { 
 				checks[i].disabled = true;
 				checks[i].checked = false;
